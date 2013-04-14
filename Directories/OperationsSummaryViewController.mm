d56bf8c5cb6c90fb1ef174401fb7510fc69ccc61
kabuto85@gmail.com <kabuto85@gmail.com@6d69d694-aee9-7f4e-a2c8-cf24c06ef0d4>
2013-04-14 16:01:55 +0000

    made operations list nicer

--- Directories/OperationsSummaryViewController.mm
+++ Directories/OperationsSummaryViewController.mm
@@ -29,11 +29,6 @@
 {
     OperationsController *m_OperationsController;
     
-    // Original frame of the operations list.
-    NSRect m_ListFrame;
-    // Original superview of the operation list.
-    NSView *m_ListSuperview;
-    
     BOOL m_ListVisible;
 }
 @synthesize OperationsController = m_OperationsController;
@@ -44,23 +39,24 @@
 }
 
 - (void)InitView
-{    
-    m_ListFrame = self.ScrollView.frame;
-    m_ListSuperview = self.ScrollView.superview;
+{
+    NSArray *colors = [NSArray arrayWithObjects:
+                       [NSColor colorWithDeviceRed:0.9 green:0.9 blue:0.9 alpha:1.0],
+                       [NSColor colorWithDeviceRed:0.86 green:0.86 blue:0.86 alpha:1.0],
+                       nil];
+    [_CollectionView setBackgroundColors:colors];
+    
+    _ScrollView.layer.cornerRadius = 3;
+    _ScrollView.layer.borderWidth = 1;
+    _ScrollView.layer.borderColor = CGColorCreateGenericRGB(0.5, 0.5, 0.5, 1);
+    
+    [_ScrollView setHidden:YES];
 }
 
 - (void)ShowList
-{
-    assert(!m_ListVisible);
-    
+{    
     const NSUInteger max_height_in_items = 5;
-    
-    // Set opaque background.
-//    NSArray *colors = [NSArray arrayWithObjects:
-//                       [NSColor colorWithDeviceRed:0.9 green:0.9 blue:0.9 alpha:1.0],
-//                       [NSColor colorWithDeviceRed:0.86 green:0.86 blue:0.86 alpha:1.0],
-//                       nil];
-//    [self.CollectionView setBackgroundColors:colors];
+    const int x_delta = 47, y_delta = -3;
     
     // Calculate height of the expanded list.
     NSUInteger item_height = self.CollectionView.itemPrototype.view.frame.size.height;
@@ -70,26 +66,25 @@
     if (height_in_items > count) height_in_items = count;
     if (height_in_items > window_height_in_items) height_in_items = window_height_in_items;
     
-    CGFloat width = self.view.frame.size.width;
-    CGFloat height = height_in_items * item_height;
+    CGFloat width = _ScrollView.frame.size.width;
+    CGFloat height = height_in_items * item_height + 2;
     
-    CGPoint origin = [self.view convertPoint:NSMakePoint(45, 0)
+    CGPoint origin = [self.view convertPoint:NSMakePoint(x_delta, y_delta)
                                       toView:self.view.window.contentView];
     origin.y -= height - self.view.frame.size.height;
-    [self.view.window.contentView addSubview:self.ScrollView];
+    //[self.view.window.contentView addSubview:self.ScrollView];
     [self.ScrollView setFrameOrigin:origin];
     
     // Apply new height.
     [self.ScrollView setFrameSize:NSMakeSize(width, height)];
     
+    [self.ScrollView setHidden:NO];
     m_ListVisible = YES;
 }
 
 - (void)HideList
-{
-    assert(m_ListVisible);
-    
-    [_ScrollView removeFromSuperview];
+{    
+    [_ScrollView setHidden:YES];
     m_ListVisible = NO;
 }
 
@@ -100,12 +95,22 @@
     {
         // Count of operations is chaged. Update current operation.
         if (m_OperationsController.Operations.count == 0)
+        {
             self.CurrentOperation = nil;
+            if (m_ListVisible)
+                [self HideList];
+        }
         else
         {
             Operation *first_op = m_OperationsController.Operations[0];
             if (self.CurrentOperation != first_op)
                 self.CurrentOperation = first_op;
+            
+            // TODO: refactor
+            const NSUInteger max_height_in_items = 5;
+            if (m_ListVisible && m_OperationsController.Operations.count < max_height_in_items)
+                [self ShowList];
+            
         }
         
         return;
@@ -121,9 +126,7 @@
         [self HideList];
     else
     {
-        // Expand list only if there is more than 1 operation.
-        if (m_OperationsController.OperationsCount > 1)
-            [self ShowList];
+        [self ShowList];
     }
 }
 
@@ -149,6 +152,7 @@
 - (void)AddViewTo:(NSView *)_parent
 {
     [_parent addSubview:self.view];
+    [_parent.window.contentView addSubview:self.ScrollView];
 }
 
 @end

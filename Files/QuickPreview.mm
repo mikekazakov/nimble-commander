eca4f3c744431a58dd00934f3f9c25c40dd65bb4
mike.kazakov@gmail.com <mike.kazakov@gmail.com@6d69d694-aee9-7f4e-a2c8-cf24c06ef0d4>
2013-10-08 06:30:19 +0000

    - minor fixes

--- Files/QuickPreview.mm
+++ Files/QuickPreview.mm
@@ -77,12 +77,21 @@ static const uint64_t g_MaxFileSizeForVFSQL = 64*1024*1024; // 64mb
         std::string path = _path;
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             if(_host->IsDirectory(path.c_str(), 0, 0))
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{ self.previewItem = nil; });
                 return;
+            }
             struct stat st;
             if(_host->Stat(path.c_str(), st, 0, 0) < 0)
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{ self.previewItem = nil; });
                 return;
+            }
             if(st.st_size > g_MaxFileSizeForVFSQL)
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{ self.previewItem = nil; });                
                 return;
+            }
             
             char tmp[MAXPATHLEN];
             if(!TemporaryNativeFileStorage::Instance().CopySingleFile(path.c_str(), _host, tmp))

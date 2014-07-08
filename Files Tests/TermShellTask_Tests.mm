8bdf9f8ff0778eae26dac3e6d02e8ff512143653
Michael G. Kazakov <Michael Kazakov>
2014-07-08 14:21:53 +0700

    - more TermShellTask tests

--- Files Tests/TermShellTask_Tests.mm
+++ Files Tests/TermShellTask_Tests.mm
@@ -17,12 +17,15 @@
 
 - (void)testBasic {
     TermShellTask shell;
+    XCTAssert( shell.State() == TermShellTask::StateInactive );
+    
     string cwd = CommonPaths::Get(CommonPaths::Home);
     shell.Launch(cwd.c_str(), 100, 100);
     usleep(100000); // 100msec should be enough for init process
     
     // check cwd
     XCTAssert( shell.CWD() == cwd );
+    XCTAssert( shell.State() == TermShellTask::StateShell);
     
     // the only task is running is shell itself, and is not returned by ChildrenList
     XCTAssert( shell.ChildrenList().empty() );
@@ -32,18 +35,49 @@
     usleep(100000);
     XCTAssert( shell.ChildrenList().size() == 1 );
     XCTAssert( shell.ChildrenList()[0] == "top" );
+    XCTAssert( shell.State() == TermShellTask::StateProgramExternal);
     
     // simulates user press Q to quit top
     shell.WriteChildInput("q", 1);
     usleep(100000);
     XCTAssert( shell.ChildrenList().empty() );
+    XCTAssert( shell.State() == TermShellTask::StateShell);
   
     // check chdir
     cwd = CommonPaths::Get(CommonPaths::Home) + "/Downloads";
     shell.ChDir( cwd.c_str() );
     usleep(100000);
-    XCTAssert( shell.CWD() == cwd );    
-}
+    XCTAssert( shell.CWD() == cwd );
+    XCTAssert( shell.State() == TermShellTask::StateShell);
+    
+    // test chdir in the middle of some typing
+    shell.WriteChildInput("ls ", 3);
+    cwd = CommonPaths::Get(CommonPaths::Home);
+    shell.ChDir( cwd.c_str() );
+    usleep(100000);
+    XCTAssert( shell.CWD() == cwd );
+    XCTAssert( shell.State() == TermShellTask::StateShell);
 
+    // check internal program state
+    shell.WriteChildInput("top\r", 4);
+    usleep(100000);
+    XCTAssert( shell.ChildrenList().size() == 1 );
+    XCTAssert( shell.ChildrenList()[0] == "top" );
+    XCTAssert( shell.State() == TermShellTask::StateProgramInternal );
+
+    // check termination
+    shell.Terminate();
+    XCTAssert( shell.ChildrenList().empty() );
+    XCTAssert( shell.State() == TermShellTask::StateInactive );
+    
+    // check execution with short path in different directory
+    shell.Launch(CommonPaths::Get(CommonPaths::Home).c_str(), 100, 100);
+    usleep(100000); // 100msec should be enough for init process
+    shell.Execute("top", "/usr/bin/", nullptr);
+    usleep(100000);
+    XCTAssert( shell.ChildrenList().size() == 1 );
+    XCTAssert( shell.ChildrenList()[0] == "top" );
+    XCTAssert( shell.State() == TermShellTask::StateProgramExternal );
+}
 
 @end

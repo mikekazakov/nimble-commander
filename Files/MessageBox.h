6d0dfac399838304e784f1c9a88666a9afe6bca6
Michael G. Kazakov <Michael Kazakov>
2014-09-16 18:13:34 +0700

    - removed unused code

--- Files/MessageBox.h
+++ Files/MessageBox.h
@@ -13,15 +13,5 @@ typedef void (^MessageBoxCompletionHandler)(int result);
 @interface MessageBox : NSAlert
 
 - (void)ShowSheetWithHandler: (NSWindow *)_for_window handler:(MessageBoxCompletionHandler)_handler;
-- (void)ShowSheet: (NSWindow *)_for_window ptr:(volatile int*)_retptr;
 
 @end
-
-
-// TODO: move it to a better place
-void MessageBoxRetryCancel(NSString *_text1, NSString *_text2, NSWindow *_wnd, volatile int *_ret);
-void MessageBoxRetrySkipCancel(NSString *_text1, NSString *_text2, NSWindow *_wnd, volatile int *_ret);
-void MessageBoxRetrySkipSkipallCancel(NSString *_text1, NSString *_text2, NSWindow *_wnd, volatile int *_ret);
-void MessageBoxOverwriteAppendCancel(NSString *_text1, NSString *_text2, NSWindow *_wnd, volatile int *_ret);
-void MessageBoxOverwriteOverwriteallAppendAppenallSkipSkipAllCancel(NSString *_text1, NSString *_text2, NSWindow *_wnd, volatile int *_ret);
-// returns NSAlertFirstButtonReturn, NSAlertSecondButtonReturn and so on

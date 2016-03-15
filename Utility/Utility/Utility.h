cf488a71b07b3c80af1972eae3489c61c6968e81
Michael Kazakov <Michael Kazakov>
2016-03-15 14:01:34 +0600

    - first build-able settings as Nimble Commander

---
+++ Utility/Utility/Utility.h
@@ -0,0 +1,19 @@
+//
+//  Utility.h
+//  Utility
+//
+//  Created by Michael G. Kazakov on 15/03/16.
+//  Copyright © 2016 MIchael Kazakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Utility.
+FOUNDATION_EXPORT double UtilityVersionNumber;
+
+//! Project version string for Utility.
+FOUNDATION_EXPORT const unsigned char UtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utility/PublicHeader.h>
+
+

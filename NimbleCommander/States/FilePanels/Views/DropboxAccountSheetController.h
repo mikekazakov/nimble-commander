f82ebfdb406cd5fd86550f3a32d265a7be1880d5
Michael G. Kazakov <migun@lucyfire.local>
2017-04-26 23:01:19 +0700

    - fighting with git

---
+++ NimbleCommander/States/FilePanels/Views/DropboxAccountSheetController.h
@@ -0,0 +1,13 @@
+//
+//  DropboxAccountSheetController.h
+//  NimbleCommander
+//
+//  Created by Michael G. Kazakov on 4/26/17.
+//  Copyright © 2017 Michael G. Kazakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DropboxAccountSheetController : NSWindowController
+
+@end
---
+++ NimbleCommander/States/FilePanels/Views/DropboxAccountSheetController.mm
@@ -0,0 +1,23 @@
+//
+//  DropboxAccountSheetController.m
+//  NimbleCommander
+//
+//  Created by Michael G. Kazakov on 4/26/17.
+//  Copyright © 2017 Michael G. Kazakov. All rights reserved.
+//
+
+#import "DropboxAccountSheetController.h"
+
+@interface DropboxAccountSheetController ()
+
+@end
+
+@implementation DropboxAccountSheetController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end

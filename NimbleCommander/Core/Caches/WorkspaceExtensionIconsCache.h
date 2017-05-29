a0c1eb4f812379b227bb955b4c7dc0a7d73cc1fa
Michael G. Kazakov <migun@lucyfire.local>
2017-05-29 09:25:49 +0700

    - simplified WorkspaceExtensionIconsCache - it doesn't check if extension icon is generic anymore

--- NimbleCommander/Core/Caches/WorkspaceExtensionIconsCache.h
+++ NimbleCommander/Core/Caches/WorkspaceExtensionIconsCache.h
@@ -5,7 +5,6 @@ class WorkspaceExtensionIconsCache
 public:
     static WorkspaceExtensionIconsCache& Instance();
 
-    // may return nil if icon is generic
     NSImage *CachedIconForExtension( const string& _extension ) const;
     NSImage *IconForExtension( const string& _extension );
 
@@ -14,11 +13,10 @@ public:
     
 private:
     WorkspaceExtensionIconsCache();
-    bool IsEqualToDefault( NSImage *_img ) const;
+    NSImage *Find_Locked( const string &_extension ) const;
+    void Commit_Locked( const string &_extension, NSImage *_image);
     mutable spinlock                m_Lock;
     unordered_map<string, NSImage*> m_Icons;
     NSImage *m_GenericFileIcon;
-    NSData  *m_GenericFileIconTIFF;
     NSImage *m_GenericFolderIcon;
-    NSData  *m_GenericFolderIconTIFF;
 };
--- NimbleCommander/Core/Caches/WorkspaceExtensionIconsCache.mm
+++ NimbleCommander/Core/Caches/WorkspaceExtensionIconsCache.mm
@@ -3,44 +3,53 @@
 WorkspaceExtensionIconsCache::WorkspaceExtensionIconsCache()
 {
     m_GenericFolderIcon = [NSImage imageNamed:NSImageNameFolder];
-    m_GenericFileIconTIFF = m_GenericFolderIcon.TIFFRepresentation;
     m_GenericFileIcon = [NSWorkspace.sharedWorkspace iconForFileType:
         NSFileTypeForHFSTypeCode(kGenericDocumentIcon)];
-    m_GenericFileIconTIFF = m_GenericFileIcon.TIFFRepresentation;
 }
 
 WorkspaceExtensionIconsCache& WorkspaceExtensionIconsCache::Instance()
 {
-    static
-    const auto i = new WorkspaceExtensionIconsCache;
+    static const auto i = new WorkspaceExtensionIconsCache;
     return *i;
 }
 
 NSImage *WorkspaceExtensionIconsCache::CachedIconForExtension( const string& _extension ) const
 {
+    return Find_Locked(_extension);
+}
+
+NSImage *WorkspaceExtensionIconsCache::Find_Locked( const string &_extension ) const
+{
+    if( _extension.empty() )
+        return nil;
+    
     LOCK_GUARD(m_Lock) {
-        auto i = m_Icons.find( _extension );
-        if( i != end(m_Icons) )
+        if( const auto i = m_Icons.find( _extension ); i != end(m_Icons) )
             return i->second;
     }
     return nil;
 }
 
-NSImage *WorkspaceExtensionIconsCache::IconForExtension( const string& _extension )
+void WorkspaceExtensionIconsCache::Commit_Locked( const string &_extension, NSImage *_image)
 {
     LOCK_GUARD(m_Lock) {
-        auto i = m_Icons.find( _extension );
-        if( i != end(m_Icons) )
-            return i->second;
+        m_Icons.emplace(_extension, _image);
     }
+}
+
+NSImage *WorkspaceExtensionIconsCache::IconForExtension( const string& _extension )
+{
+    if( _extension.empty() )
+        return nil;
     
-    const auto e = [NSString stringWithUTF8StdStringNoCopy:_extension];
-    const auto image = [NSWorkspace.sharedWorkspace iconForFileType:e];
-    const auto is_default = IsEqualToDefault(image);
-    LOCK_GUARD(m_Lock) {
-        m_Icons.emplace(_extension, !is_default ? image : nil);
-    }
-    return !is_default ? image : nil;
+    if( const auto existing = Find_Locked(_extension) )
+        return existing;
+
+    const auto extension = [NSString stringWithUTF8StdString:_extension];
+    const auto image = [NSWorkspace.sharedWorkspace iconForFileType:extension];
+    Commit_Locked( _extension, image );
+    
+    return image;
 }
 
 NSImage *WorkspaceExtensionIconsCache::GenericFileIcon() const
@@ -51,11 +60,4 @@ NSImage *WorkspaceExtensionIconsCache::GenericFileIcon() const
 NSImage *WorkspaceExtensionIconsCache::GenericFolderIcon() const
 {
     return m_GenericFolderIcon;
-}
-
-bool WorkspaceExtensionIconsCache::IsEqualToDefault( NSImage *_img ) const
-{
-    auto tiff = _img.TIFFRepresentation;
-    return [tiff isEqualToData:m_GenericFileIconTIFF] ||
-           [tiff isEqualToData:m_GenericFolderIconTIFF];
 }

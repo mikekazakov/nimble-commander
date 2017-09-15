d932227eda26d68fbd701907b3a4407955b92eeb
Michael G. Kazakov <mike.kazakov@gmail.com>
2017-09-15 19:12:14 +0700

    - updating tests

--- Operations/tests/Deletion_Tests.mm
+++ Operations/tests/Deletion_Tests.mm
@@ -1,6 +1,7 @@
 #import <XCTest/XCTest.h>
 #include <VFS/VFS.h>
 #include <VFS/Native.h>
+#include <VFS/NetFTP.h>
 #include "../source/Deletion/Deletion.h"
 
 using namespace nc::ops;
@@ -109,6 +110,75 @@ static vector<VFSListingItem> FetchItems(const string& _directory_path,
     operation.Start();
     operation.Wait();
     XCTAssert( operation.State() != OperationState::Completed );
+}
+
+- (void)testComplexDeletion
+{
+    XCTAssert( VFSEasyCopyNode("/Applications/Mail.app",
+                               m_NativeHost,
+                               (path(m_TmpDir) / "Mail.app").c_str(),
+                               m_NativeHost) == 0);
+    
+    Deletion operation{ FetchItems(m_TmpDir.native(),
+                                   {"Mail.app"},
+                                   *m_NativeHost),
+                        DeletionType::Permanent };
+    operation.Start();
+    operation.Wait();
+    XCTAssert( operation.State() == OperationState::Completed );
+    
+    XCTAssert( !m_NativeHost->Exists((path(m_TmpDir) / "Mail.app").c_str()) );
+}
+
+- (void)testSimpleDeleteFromFTP
+{
+    try {
+        auto host = make_shared<VFSNetFTPHost>("192.168.2.5", "", "", "/");
+        
+        const char *fn1 = "/System/Library/Kernels/kernel", *fn2 = "/Public/!FilesTesting/mach_kernel";
+        VFSStat stat;
+        
+        // if there's a trash from previous runs - remove it
+        if( host->Stat(fn2, stat, 0, 0) == 0)
+            XCTAssert( host->Unlink(fn2, 0) == 0);
+        
+        XCTAssert( VFSEasyCopyFile(fn1, VFSNativeHost::SharedHost(), fn2, host) == 0);
+        
+        Deletion operation{ FetchItems("/Public/!FilesTesting", {"mach_kernel"}, *host),
+            DeletionType::Permanent };
+        operation.Start();
+        operation.Wait();
+        
+        XCTAssert( host->Stat(fn2, stat, 0, 0) != 0); // check that file has gone
+    } catch (VFSErrorException &e) {
+        XCTAssert( e.code() == 0 );
+    }
+}
+
+- (void)testDeletingFromFTPDirectory
+{
+    try {
+        auto host = make_shared<VFSNetFTPHost>("192.168.2.5", "", "", "/");
+        
+        const char *fn1 = "/bin", *fn2 = "/Public/!FilesTesting/bin";
+        VFSStat stat;
+        
+        // if there's a trash from previous runs - remove it
+        if( host->Stat(fn2, stat, 0, 0) == 0)
+            XCTAssert(VFSEasyDelete(fn2, host) == 0);
+        
+        XCTAssert( VFSEasyCopyNode(fn1, VFSNativeHost::SharedHost(), fn2, host) == 0);
+        
+        
+        Deletion operation{ FetchItems("/Public/!FilesTesting", {"bin"}, *host),
+            DeletionType::Permanent };
+        operation.Start();
+        operation.Wait();
+        
+        XCTAssert( host->Stat(fn2, stat, 0, 0) != 0); // check that file has gone
+    } catch (VFSErrorException &e) {
+        XCTAssert( e.code() == 0 );
+    }
 }
 
 - (path)makeTmpDir

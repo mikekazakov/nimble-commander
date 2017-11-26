8c1b242811ebe3df3f1ffc1a9a40889362b06dd4
Michael Kazakov <migun@Michaels-Mac-mini.local>
2017-11-26 11:43:08 +0700

    - updated tests

--- NimbleCommander/Core/FileWindow_Tests.mm
+++ NimbleCommander/Core/FileWindow_Tests.mm
@@ -15,7 +15,7 @@ public:
                               ReadParadigm _behave_as);
     
     
-    virtual int     Open(int _open_flags, const VFSCancelChecker &_cancel_checker) override;
+    virtual int     Open(unsigned long _open_flags, const VFSCancelChecker &_cancel_checker) override;
     virtual bool    IsOpened() const override {return m_Opened;}
     virtual int     Close() override;
     
@@ -139,7 +139,7 @@ bool TestGenericMemReadOnlyFile::Eof() const
     return m_Pos == m_Size;
 }
 
-int TestGenericMemReadOnlyFile::Open(int _open_flags, const VFSCancelChecker &_cancel_checker)
+int TestGenericMemReadOnlyFile::Open(unsigned long _open_flags, const VFSCancelChecker &_cancel_checker)
 {
     m_Opened = true;
     return 0;

a8ed9d29f33303314eafacccef83f8df86a14a05
Michael Kazakov <mike.kazakov@gmail.com>
2019-01-06 20:53:01 +0000

    test scripts rearrangement

--- Scripts/run_all_unit_tests.sh
+++ Scripts/run_all_unit_tests.sh
@@ -9,13 +9,25 @@ build_target()
         -project ../NimbleCommander.xcodeproj \
         -scheme $TARGET \
         -configuration Debug \
-        -parallelizeTargets"    
+        -parallelizeTargets \
+        -quiet"
     BINARY_DIR=$($XC -showBuildSettings | grep " BUILT_PRODUCTS_DIR =" | sed -e 's/.*= *//')
     BINARY_NAME=$($XC -showBuildSettings | grep " FULL_PRODUCT_NAME =" | sed -e 's/.*= *//')
     BINARY_PATH=$BINARY_DIR/$BINARY_NAME    
     $XC build
 }
 
+build_target ConfigUT
+$BINARY_PATH
+
 build_target UtilityUT
 $BINARY_PATH
 
+build_target VFSIconUnitTests
+$BINARY_PATH
+
+build_target VFSIconIntegrationTests
+$BINARY_PATH
+
+build_target VFSUT
+$BINARY_PATH

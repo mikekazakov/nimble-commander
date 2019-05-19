a395e5d685d023ab93bc0363535b13eab18c34b6
Michael Kazakov <mike.kazakov@gmail.com>
2019-05-19 14:49:45 +0100

    added configs to Habanero, created HabaneroUT target

---
+++ Habanero/tests/UnitTests_main.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+
+#define GTEST_DONT_DEFINE_FAIL 1
+#define GTEST_DONT_DEFINE_SUCCEED 1
+#include <gmock/gmock.h>
+
+int main( int argc, char* argv[] ) {
+    ::testing::GTEST_FLAG(throw_on_failure) = true;
+    ::testing::InitGoogleMock(&argc, argv);
+    int result = Catch::Session().run( argc, argv );    
+    return result;
+}

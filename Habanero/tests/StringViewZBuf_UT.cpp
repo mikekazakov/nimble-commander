a69d87e9e95d8dd7756c761f0afa0c43ffd2b646
Michael Kazakov <mike.kazakov@gmail.com>
2020-01-04 09:56:54 +0000

    - added StringViewZBuf

---
+++ Habanero/tests/StringViewZBuf_UT.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2020 Michael Kazakov. Subject to GNU General Public License version 3.
+#include <Habanero/StringViewZBuf.h>
+#include "UnitTests_main.h"
+
+#define PREFIX "StringViewZBuf "
+
+using nc::base::StringViewZBuf;
+using namespace std;
+
+TEST_CASE(PREFIX"Empty")
+{
+    std::string_view v{""};
+    StringViewZBuf<1> zb{v};
+    CHECK( v == zb.c_str() );
+}
+
+TEST_CASE(PREFIX"Short")
+{
+    std::string_view v{"abcd"};
+    StringViewZBuf<5> zb{v};
+    CHECK( v == zb.c_str() );
+}
+
+TEST_CASE(PREFIX"Long")
+{
+    std::string_view v{"abcde"};
+    StringViewZBuf<5> zb{v};
+    CHECK( v == zb.c_str() );
+}

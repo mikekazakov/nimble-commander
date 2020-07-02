f0a8cf3cd84665b7cd8777c142749decbf07db49
Michael Kazakov <mike.kazakov@gmail.com>
2020-07-02 11:24:09 +0100

    - tests reorg

--- Term/tests/Term_IT.cpp
+++ Term/tests/Term_IT.cpp
@@ -339,86 +339,7 @@ TEST_CASE(PREFIX"UTF cases")
     }
 }
 
-TEST_CASE(PREFIX"vttest - test of cursor movements, "
-"Test of cursor-control characters inside ESC sequences.")
-{
-    const auto raw_input =
-    "\x1B[?3l\x1B[2J\x1B[1;1HTest of cursor-control characters inside ESC sequences."
-    "\x0D\x0D\x0A""Below should be four identical lines:\x0D\x0D\x0A\x0D\x0D\x0A""A B C D E F G H I"
-    "\x0D\x0D\x0A""A\x1B[2\x08""CB\x1B[2\x08""CC\x1B[2\x08""CD\x1B[2\x08""CE\x1B[2\x08""CF\x1B[2\x08""CG\x1B[2\x08""CH\x1B[2\x08""CI"
-    "\x1B[2\x08""C\x0D\x0D\x0A""A \x1B[\x0D""2CB\x1B[\x0D""4CC\x1B[\x0D""6CD\x1B[\x0D""8CE\x1B[\x0D""10CF\x1B[\x0D""12CG\x1B[\x0D""14CH\x1B[\x0D""16CI"
-    "\x0D\x0D\x0A\x1B[20lA \x1B[1\x0B""AB \x1B[1\x0B""AC \x1B[1\x0B""AD \x1B[1\x0B""AE \x1B[1\x0B""AF \x1B[1\x0B""AG \x1B[1\x0B""AH \x1B[1\x0B""AI"
-    " \x1B[1\x0BA\x0D\x0D\x0A\x0D\x0D\x0A""Push <RETURN>";
-    const auto expectation = 
-    "Test of cursor-control characters inside ESC sequences.     "
-    "Below should be four identical lines:                       "
-    "                                                            "
-    "A B C D E F G H I                                           "
-    "A B C D E F G H I                                           "
-    "A B C D E F G H I                                           "
-    "A B C D E F G H I                                           "
-    "                                                            "
-    "Push <RETURN>                                               ";
-
-    Parser2Impl parser;
-    Screen screen(60, 9);
-    InterpreterImpl interpreter(screen);    
-    const auto input = std::string_view{raw_input};
-    const auto input_bytes = Parser2::Bytes(reinterpret_cast<const std::byte*>(input.data()),
-                                            input.length());
-    interpreter.Interpret(parser.Parse( input_bytes ) );
-    const auto result = screen.Buffer().DumpScreenAsANSI();
-    CHECK( result == expectation );
-}
-
-TEST_CASE(PREFIX"vttest - test of cursor movements, "
-"Test of leading zeros in ESC sequences.")
-{
-    const auto raw_input =
-    "\x1B[2J\x1B[1;1HTest of leading zeros in ESC sequences.\x0D\x0D\x0A"
-    "Two lines below you should see the sentence \"This is a correct sentence\"."   
-    "\x1B[00000000004;000000001HT\x1B[00000000004;000000002Hh\x1B[00000000004;000000003Hi\x1B[00000000004;000000004Hs"
-    "\x1B[00000000004;000000005H \x1B[00000000004;000000006Hi\x1B[00000000004;000000007Hs\x1B[00000000004;000000008H "
-    "\x1B[00000000004;000000009Ha\x1B[00000000004;0000000010H \x1B[00000000004;0000000011Hc\x1B[00000000004;0000000012Ho"
-    "\x1B[00000000004;0000000013Hr\x1B[00000000004;0000000014Hr\x1B[00000000004;0000000015He\x1B[00000000004;0000000016Hc"
-    "\x1B[00000000004;0000000017Ht\x1B[00000000004;0000000018H \x1B[00000000004;0000000019Hs\x1B[00000000004;0000000020He"
-    "\x1B[00000000004;0000000021Hn\x1B[00000000004;0000000022Ht\x1B[00000000004;0000000023He\x1B[00000000004;0000000024Hn"
-    "\x1B[00000000004;0000000025Hc\x1B[00000000004;0000000026He\x1B[20;1H"    
-    "Push <RETURN>";    
-    const auto expectation = 
-    "Test of leading zeros in ESC sequences.                                         "
-    "Two lines below you should see the sentence \"This is a correct sentence\".       "
-    "                                                                                "
-    "This is a correct sentence                                                      "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "                                                                                "
-    "Push <RETURN>                                                                   ";
-
-    Parser2Impl parser;
-    Screen screen(80, 20);
-    InterpreterImpl interpreter(screen);    
-    const auto input = std::string_view{raw_input};
-    const auto input_bytes = Parser2::Bytes(reinterpret_cast<const std::byte*>(input.data()),
-                                            input.length());
-    interpreter.Interpret(parser.Parse( input_bytes ) );
-    const auto result = screen.Buffer().DumpScreenAsANSI();
-    CHECK( result == expectation );
-}
-
-TEST_CASE(PREFIX"vttest - test of cursor movements, "
+TEST_CASE(PREFIX"vttest(1.1) - test of cursor movements, "
 "Test zero movements, display alignment, display erase, line erase")
 {
     const auto raw_input =
@@ -540,7 +461,7 @@ TEST_CASE(PREFIX"vttest - test of cursor movements, "
     "*+                                                                            +*"
     "*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*"
     "********************************************************************************"
-    "                                                                                ";    
+    "                                                                                ";
     
     Parser2Impl parser;
     Screen screen(80, 25);
@@ -553,7 +474,7 @@ TEST_CASE(PREFIX"vttest - test of cursor movements, "
     CHECK( result == expectation );
 }
 
-TEST_CASE(PREFIX"vttest - test of cursor movements, "
+TEST_CASE(PREFIX"vttest(1.3) - test of cursor movements, "
 "autowrap, mixing control and print characters")
 {
     const auto raw_input =
@@ -607,6 +528,85 @@ TEST_CASE(PREFIX"vttest - test of cursor movements, "
     const auto input_bytes = Parser2::Bytes(reinterpret_cast<const std::byte*>(input.data()),
                                             input.length());
     interpreter.Interpret( parser.Parse( input_bytes ) );
+    const auto result = screen.Buffer().DumpScreenAsANSI();
+    CHECK( result == expectation );
+}
+
+TEST_CASE(PREFIX"vttest(1.5) - test of cursor movements, "
+"Test of cursor-control characters inside ESC sequences.")
+{
+    const auto raw_input =
+    "\x1B[?3l\x1B[2J\x1B[1;1HTest of cursor-control characters inside ESC sequences."
+    "\x0D\x0D\x0A""Below should be four identical lines:\x0D\x0D\x0A\x0D\x0D\x0A""A B C D E F G H I"
+    "\x0D\x0D\x0A""A\x1B[2\x08""CB\x1B[2\x08""CC\x1B[2\x08""CD\x1B[2\x08""CE\x1B[2\x08""CF\x1B[2\x08""CG\x1B[2\x08""CH\x1B[2\x08""CI"
+    "\x1B[2\x08""C\x0D\x0D\x0A""A \x1B[\x0D""2CB\x1B[\x0D""4CC\x1B[\x0D""6CD\x1B[\x0D""8CE\x1B[\x0D""10CF\x1B[\x0D""12CG\x1B[\x0D""14CH\x1B[\x0D""16CI"
+    "\x0D\x0D\x0A\x1B[20lA \x1B[1\x0B""AB \x1B[1\x0B""AC \x1B[1\x0B""AD \x1B[1\x0B""AE \x1B[1\x0B""AF \x1B[1\x0B""AG \x1B[1\x0B""AH \x1B[1\x0B""AI"
+    " \x1B[1\x0BA\x0D\x0D\x0A\x0D\x0D\x0A""Push <RETURN>";
+    const auto expectation = 
+    "Test of cursor-control characters inside ESC sequences.     "
+    "Below should be four identical lines:                       "
+    "                                                            "
+    "A B C D E F G H I                                           "
+    "A B C D E F G H I                                           "
+    "A B C D E F G H I                                           "
+    "A B C D E F G H I                                           "
+    "                                                            "
+    "Push <RETURN>                                               ";
+
+    Parser2Impl parser;
+    Screen screen(60, 9);
+    InterpreterImpl interpreter(screen);    
+    const auto input = std::string_view{raw_input};
+    const auto input_bytes = Parser2::Bytes(reinterpret_cast<const std::byte*>(input.data()),
+                                            input.length());
+    interpreter.Interpret(parser.Parse( input_bytes ) );
+    const auto result = screen.Buffer().DumpScreenAsANSI();
+    CHECK( result == expectation );
+}
+
+TEST_CASE(PREFIX"vttest(1.6) - test of cursor movements, "
+"Test of leading zeros in ESC sequences.")
+{
+    const auto raw_input =
+    "\x1B[2J\x1B[1;1HTest of leading zeros in ESC sequences.\x0D\x0D\x0A"
+    "Two lines below you should see the sentence \"This is a correct sentence\"."   
+    "\x1B[00000000004;000000001HT\x1B[00000000004;000000002Hh\x1B[00000000004;000000003Hi\x1B[00000000004;000000004Hs"
+    "\x1B[00000000004;000000005H \x1B[00000000004;000000006Hi\x1B[00000000004;000000007Hs\x1B[00000000004;000000008H "
+    "\x1B[00000000004;000000009Ha\x1B[00000000004;0000000010H \x1B[00000000004;0000000011Hc\x1B[00000000004;0000000012Ho"
+    "\x1B[00000000004;0000000013Hr\x1B[00000000004;0000000014Hr\x1B[00000000004;0000000015He\x1B[00000000004;0000000016Hc"
+    "\x1B[00000000004;0000000017Ht\x1B[00000000004;0000000018H \x1B[00000000004;0000000019Hs\x1B[00000000004;0000000020He"
+    "\x1B[00000000004;0000000021Hn\x1B[00000000004;0000000022Ht\x1B[00000000004;0000000023He\x1B[00000000004;0000000024Hn"
+    "\x1B[00000000004;0000000025Hc\x1B[00000000004;0000000026He\x1B[20;1H"    
+    "Push <RETURN>";    
+    const auto expectation = 
+    "Test of leading zeros in ESC sequences.                                         "
+    "Two lines below you should see the sentence \"This is a correct sentence\".       "
+    "                                                                                "
+    "This is a correct sentence                                                      "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "                                                                                "
+    "Push <RETURN>                                                                   ";
+
+    Parser2Impl parser;
+    Screen screen(80, 20);
+    InterpreterImpl interpreter(screen);    
+    const auto input = std::string_view{raw_input};
+    const auto input_bytes = Parser2::Bytes(reinterpret_cast<const std::byte*>(input.data()),
+                                            input.length());
+    interpreter.Interpret(parser.Parse( input_bytes ) );
     const auto result = screen.Buffer().DumpScreenAsANSI();
     CHECK( result == expectation );
 }

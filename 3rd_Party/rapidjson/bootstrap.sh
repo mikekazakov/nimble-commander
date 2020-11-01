03a391b234efb03215e2882c1e480fa0981ef7a8
Michael Kazakov <mike.kazakov@gmail.com>
2020-11-01 09:22:59 +0000

    updated rapidjson

---
+++ 3rd_Party/rapidjson/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -o pipefail
+set -o xtrace
+
+CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+TMP_DIR=${CUR_DIR}/rapidjson.tmp
+
+mkdir ${TMP_DIR}
+cd ${TMP_DIR} 
+
+git clone -b master --single-branch https://github.com/Tencent/rapidjson
+
+cd ..
+
+rm -rf ./include/
+
+cp -R ${TMP_DIR}/rapidjson/include .
+
+rm -rf ${TMP_DIR}

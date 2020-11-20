abcb61a0661c817e85496368bcf7dd3731cdec22
Michael Kazakov <mike.kazakov@gmail.com>
2020-11-20 16:58:17 +0000

    bz2 is properly bootstrapped

--- 3rd_Party/bootstrap.sh
+++ 3rd_Party/bootstrap.sh
@@ -5,4 +5,5 @@ set -o xtrace
 # pour yourself some coffee and relax, it'll take a while
 export MACOSX_DEPLOYMENT_TARGET="10.15"
 ./z/bootstrap.sh
+./bz2/bootstrap.sh
 ./spdlog/bootstrap.sh
---
+++ 3rd_Party/bz2/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -o pipefail
+set -o xtrace
+
+CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+TMP_DIR=${CUR_DIR}/bz2.tmp
+
+mkdir ${TMP_DIR}
+cd ${TMP_DIR} 
+
+git clone -b bzip2-1.0.8 --single-branch https://sourceware.org/git/bzip2.git
+cd bzip2
+make 
+cd ../..
+
+rm -rf ./include/
+rm -rf ./built/
+mkdir include
+mkdir built
+
+cp ${TMP_DIR}/bzip2/libbz2.a ./built/
+cp ${TMP_DIR}/bzip2/bzlib.h ./include/
+
+rm -rf ${TMP_DIR} 

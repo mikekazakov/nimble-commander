e5a5e6f669100fcae6e37472f914c863d15d2b6d
Michael Kazakov <mike.kazakov@gmail.com>
2020-12-01 20:55:06 +0000

    - ""

---
+++ CUI/include/CUI/FilterPopUpMenu.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2017-2018 Michael Kazakov. Subject to GNU General Public License version 3.
+#pragma once
+
+#include <Cocoa/Cocoa.h>
+
+@interface FilterPopUpMenu : NSMenu
+
+- (instancetype)initWithTitle:(NSString *)title;
+
+@end

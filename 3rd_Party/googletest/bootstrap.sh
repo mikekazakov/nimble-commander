e6cf6f6917da210a74ae6b31a37cc00de1c79bd8
Michael Kazakov <mike.kazakov@gmail.com>
2020-12-04 13:41:33 +0000

    updated gtest for universal binary

---
+++ 3rd_Party/googletest/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -o pipefail
+set -o xtrace
+set -e
+
+CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+TMP_DIR=${CUR_DIR}/gtest.tmp
+
+mkdir ${TMP_DIR}
+cd ${TMP_DIR} 
+
+git clone -b release-1.10.0 --single-branch https://github.com/google/googletest.git
+cd googletest
+
+mkdir build && cd build
+cmake \
+  -D CMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -D CMAKE_OSX_DEPLOYMENT_TARGET="10.15" \
+  -D CMAKE_CXX_FLAGS="-fvisibility=hidden -flto -Os" \
+  ..
+make -j
+
+cd ../../..
+
+rm -rf ./include/
+rm -rf ./lib/
+mkdir include
+mkdir lib
+
+cp ${TMP_DIR}/googletest/build/lib/*.a ./lib/
+cp -R ${TMP_DIR}/googletest/googlemock/include/* ./include/
+cp -R ${TMP_DIR}/googletest/googletest/include/* ./include/
+
+rm -rf ${TMP_DIR} 

f3d5449fb9a6c8b9c3c638e74acafcc17c6dd64b
Michael Kazakov <mike.kazakov@gmail.com>
2020-12-24 11:00:27 +0000

    - automatically expanding panels upon Preview request

--- NimbleCommander/States/FilePanels/Actions/ShowQuickLook.mm
+++ NimbleCommander/States/FilePanels/Actions/ShowQuickLook.mm
@@ -1,29 +1,34 @@
-// Copyright (C) 2017-2019 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2017-2020 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "ShowQuickLook.h"
 #include "../PanelController.h"
 #include "../PanelView.h"
 #include "../MainWindowFilePanelState.h"
+#include "../Views/FilePanelMainSplitView.h"
 #include "../PanelAux.h"
 
 namespace nc::panel::actions {
 
-bool ShowQuickLook::Predicate( PanelController *_target ) const
+bool ShowQuickLook::Predicate(PanelController *_target) const
 {
     if( !_target.view.item )
         return false;
 
-    if( ShowQuickLookAsFloatingPanel() )
-        return true;
-    
-    if( _target.state.anyPanelCollapsed )
-        return false;
-    
     return true;
 }
-        
-void ShowQuickLook::Perform( PanelController *_target, [[maybe_unused]] id _sender ) const
+
+void ShowQuickLook::Perform(PanelController *_target, [[maybe_unused]] id _sender) const
 {
     const auto state = _target.state;
+
+    if( ShowQuickLookAsFloatingPanel() == false ) {
+        if( state.anyPanelCollapsed ) {
+            if( [state isLeftController:_target] )
+                [state.splitView expandRightView];
+            else if( [state isRightController:_target] )
+                [state.splitView expandLeftView];
+        }
+    }
+
     if( [state quickLookForPanel:_target make:false] ) {
         [state closeAttachedUI:_target];
     }
@@ -32,5 +37,5 @@ void ShowQuickLook::Perform( PanelController *_target, [[maybe_unused]] id _send
             [_target updateAttachedQuickLook];
     }
 }
-    
+
 }

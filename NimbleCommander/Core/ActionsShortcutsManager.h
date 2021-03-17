379a9f2591cb85d3716eae1d3328cae1f1f5b367
Michael Kazakov <mike.kazakov@gmail.com>
2021-03-17 20:55:08 +0000

    std::unordered_map -> robin_hood::unordered_map

--- NimbleCommander/Core/ActionsShortcutsManager.h
+++ NimbleCommander/Core/ActionsShortcutsManager.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2014-2018 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2014-2021 Michael Kazakov. Subject to GNU General Public License version 3.
 #pragma once
 
 // ⇧ - NSShiftKeyMask
@@ -10,6 +10,7 @@
 #include <Habanero/Observable.h>
 #include <Utility/ActionShortcut.h>
 #include <unordered_map>
+#include <robin_hood.h>
 #include <vector>
 #include <Cocoa/Cocoa.h>
 
@@ -19,20 +20,20 @@ public:
     using ShortCut = nc::utility::ActionShortcut;
     struct AutoUpdatingShortCut;
     class ShortCutsUpdater;
-    
+
     static ActionsShortcutsManager &Instance();
-    
+
     /**
      * Return -1 on if tag corresponing _action wasn't found.
      */
     int TagFromAction(const std::string &_action) const;
     int TagFromAction(const char *_action) const;
-    
+
     /**
      * return "" on if action corresponing _tag wasn't found.
      */
     std::string ActionFromTag(int _tag) const;
-    
+
     /**
      * Return default if can't be found.
      * Overrides has priority over defaults.
@@ -49,43 +50,46 @@ public:
      * Return default if can't be found.
      */
     ShortCut DefaultShortCutFromTag(int _tag) const;
-    
+
     void RevertToDefaults();
-    
-    bool SetShortCutOverride(const std::string &_action, const ShortCut& _sc);
-    
+
+    bool SetShortCutOverride(const std::string &_action, const ShortCut &_sc);
+
     void SetMenuShortCuts(NSMenu *_menu) const;
-    
-    const std::vector<std::pair<const char*,int>>& AllShortcuts() const;
-    
+
+    const std::vector<std::pair<const char *, int>> &AllShortcuts() const;
+
     using ObservationTicket = ObservableBase::ObservationTicket;
     ObservationTicket ObserveChanges(std::function<void()> _callback);
-    
+
 private:
     ActionsShortcutsManager();
-    ActionsShortcutsManager(const ActionsShortcutsManager&) = delete;
-    
+    ActionsShortcutsManager(const ActionsShortcutsManager &) = delete;
+
     void ReadOverrideFromConfig();
     void WriteOverridesToConfig() const;
-    
-    std::unordered_map<int, const char*> m_TagToAction;
-    std::unordered_map<std::string, int>      m_ActionToTag;
-    std::unordered_map<int, ShortCut>    m_ShortCutsDefaults;
-    std::unordered_map<int, ShortCut>    m_ShortCutsOverrides;
+
+    robin_hood::unordered_map<int, const char *> m_TagToAction;
+    robin_hood::unordered_map<std::string, int> m_ActionToTag;
+    robin_hood::unordered_map<int, ShortCut> m_ShortCutsDefaults;
+    robin_hood::unordered_map<int, ShortCut> m_ShortCutsOverrides;
 };
 
 class ActionsShortcutsManager::ShortCutsUpdater
 {
 public:
-    ShortCutsUpdater(std::initializer_list<ShortCut*> _hotkeys,
-                     std::initializer_list<const char*> _actions );
+    ShortCutsUpdater(std::initializer_list<ShortCut *> _hotkeys,
+                     std::initializer_list<const char *> _actions);
+
 private:
     void CheckAndUpdate() const;
-    std::vector< std::pair<ShortCut*, int> >  m_Pets;
-    ObservationTicket               m_Ticket;
+    std::vector<std::pair<ShortCut *, int>> m_Pets;
+    ObservationTicket m_Ticket;
 };
 
-#define IF_MENU_TAG_TOKENPASTE(x, y) x ## y
+#define IF_MENU_TAG_TOKENPASTE(x, y) x##y
 #define IF_MENU_TAG_TOKENPASTE2(x, y) IF_MENU_TAG_TOKENPASTE(x, y)
-#define IF_MENU_TAG(str) static const int IF_MENU_TAG_TOKENPASTE2(__tag_no_, __LINE__) = ActionsShortcutsManager::Instance().TagFromAction(str); \
+#define IF_MENU_TAG(str)                                                                           \
+    static const int IF_MENU_TAG_TOKENPASTE2(__tag_no_, __LINE__) =                                \
+        ActionsShortcutsManager::Instance().TagFromAction(str);                                    \
     if( tag == IF_MENU_TAG_TOKENPASTE2(__tag_no_, __LINE__) )

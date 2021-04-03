dfbedf29026096507fb460add3a18d191772be7c
Michael Kazakov <mike.kazakov@gmail.com>
2021-04-03 09:42:47 +0100

    - adapting for C++ casts

--- CUI/source/FilterPopUpMenu.mm
+++ CUI/source/FilterPopUpMenu.mm
@@ -1,27 +1,26 @@
-// Copyright (C) 2017-2020 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2017-2021 Michael Kazakov. Subject to GNU General Public License version 3.
 #include <Carbon/Carbon.h>
 #include "FilterPopUpMenu.h"
 #include <Utility/ObjCpp.h>
 #include <vector>
 #include <array>
 
-@interface FilterPopUpMenu()
+@interface FilterPopUpMenu ()
 
-- (void) updateFilter:(NSString*)_filter;
-
-@end
-
-@interface FilterPopUpMenuItem : NSView<NSTextFieldDelegate>
-
-@property (nonatomic) NSString *title;
+- (void)updateFilter:(NSString *)_filter;
 
 @end
 
-@implementation FilterPopUpMenu
-{
-    NSString           *m_Filter;
-    std::vector<NSString*>m_KeyEquivalents;
-    bool                m_KeyEquivalentsHidden;
+@interface FilterPopUpMenuItem : NSView <NSTextFieldDelegate>
+
+@property(nonatomic) NSString *title;
+
+@end
+
+@implementation FilterPopUpMenu {
+    NSString *m_Filter;
+    std::vector<NSString *> m_KeyEquivalents;
+    bool m_KeyEquivalentsHidden;
 }
 
 - (instancetype)init
@@ -34,10 +33,10 @@
     self = [super initWithTitle:_title];
     if( self ) {
         m_KeyEquivalentsHidden = false;
-    
+
         auto header_view = [[FilterPopUpMenuItem alloc] initWithFrame:NSMakeRect(0, 0, 220, 21)];
         header_view.title = _title;
-        
+
         auto header_item = [[NSMenuItem alloc] init];
         header_item.title = _title;
         header_item.view = header_view;
@@ -46,47 +45,46 @@
     return self;
 }
 
-- (void) updateFilter:(NSString*)_filter
+- (void)updateFilter:(NSString *)_filter
 {
     m_Filter = _filter;
     auto items = self.itemArray;
-    for( int i = 1, e = (int)items.count; i < e; ++i ) {
+    for( size_t i = 1, e = items.count; i < e; ++i ) {
         auto item = [items objectAtIndex:i];
-        
+
         if( item.hidden && item.allowsKeyEquivalentWhenHidden )
             continue;
         item.hidden = ![self validateItem:item];
     }
 
     [self updateEquivalents];
-    
+
     if( self.highlightedItem == nil || self.highlightedItem.hidden ) {
         NSMenuItem *item_to_highlight = nil;
-        for( int i = 1, e = (int)items.count; i < e; ++i ) {
+        for( size_t i = 1, e = items.count; i < e; ++i ) {
             auto item = [items objectAtIndex:i];
             if( !item.hidden && item.enabled ) {
                 item_to_highlight = item;
                 break;
             }
         }
-        
+
         if( item_to_highlight )
             [self higlightCustomItem:item_to_highlight];
     }
 }
 
-- (bool) validateItem:(NSMenuItem*)_item
+- (bool)validateItem:(NSMenuItem *)_item
 {
-    if( !m_Filter || m_Filter.length == 0)
+    if( !m_Filter || m_Filter.length == 0 )
         return true;
     if( _item.isSeparatorItem || !_item.enabled )
         return false;
-    
-    return [_item.title rangeOfString:m_Filter
-                              options:NSCaseInsensitiveSearch].length > 0;
+
+    return [_item.title rangeOfString:m_Filter options:NSCaseInsensitiveSearch].length > 0;
 }
 
-- (void) updateEquivalents
+- (void)updateEquivalents
 {
     if( m_KeyEquivalents.empty() )
         for( NSMenuItem *i in self.itemArray )
@@ -94,9 +92,9 @@
 
     if( m_Filter.length == 0 && m_KeyEquivalentsHidden == true ) {
         auto items = self.itemArray;
-        for( int i1 = 0, e1 = (int)items.count, i2 = 0, e2 = (int)m_KeyEquivalents.size();
-            i1 != e1 && i2 != e2;
-            ++i1, ++i2)
+        for( size_t i1 = 0, e1 = items.count, i2 = 0, e2 = m_KeyEquivalents.size();
+             i1 != e1 && i2 != e2;
+             ++i1, ++i2 )
             [items objectAtIndex:i1].keyEquivalent = m_KeyEquivalents[i2];
         m_KeyEquivalentsHidden = false;
     }
@@ -107,7 +105,7 @@
     }
 }
 
-- (void) higlightCustomItem:(NSMenuItem*)_item
+- (void)higlightCustomItem:(NSMenuItem *)_item
 {
     static const auto selHighlightItem = NSSelectorFromString(@"highlightItem:");
     if( [self respondsToSelector:selHighlightItem] ) {
@@ -120,17 +118,15 @@
 
 @end
 
-
-@implementation FilterPopUpMenuItem
-{
+@implementation FilterPopUpMenuItem {
     NSTextField *m_Title;
     NSTextField *m_Query;
     EventHandlerRef m_EventHandler;
 }
 
-- (instancetype) initWithFrame:(NSRect)frameRect
+- (instancetype)initWithFrame:(NSRect)frameRect
 {
-    if( self = [super initWithFrame:frameRect]) {
+    if( self = [super initWithFrame:frameRect] ) {
         m_EventHandler = nullptr;
         self.autoresizingMask = NSViewWidthSizable;
 
@@ -162,50 +158,47 @@
         [self addSubview:m_Query];
 
         auto views = NSDictionaryOfVariableBindings(m_Title, m_Query);
-        [self addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"|-(==21)-[m_Title]-[m_Query]-(==10)-|"
-                              options:0
-                              metrics:nil
-                              views:views]];
-        [self addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"V:|[m_Query]-(==1)-|"
-                              options:0
-                              metrics:nil
-                              views:views]];
-        [self addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"V:|[m_Title]-(==1)-|"
-                              options:0
-                              metrics:nil
-                              views:views]];
+        [self
+            addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"|-(==21)-[m_Title]-[m_Query]-(==10)-|"
+                                                   options:0
+                                                   metrics:nil
+                                                     views:views]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[m_Query]-(==1)-|"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:views]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[m_Title]-(==1)-|"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:views]];
     }
     return self;
 }
 
-- (void) setTitle:(NSString *)title
+- (void)setTitle:(NSString *)title
 {
     m_Title.stringValue = title;
 }
 
-- (NSString*)title
+- (NSString *)title
 {
     return m_Title.stringValue;
 }
 
-static OSStatus CarbonCallback(EventHandlerCallRef _handler,
-                               EventRef _event,
-                               void *_user_data)
+static OSStatus CarbonCallback(EventHandlerCallRef _handler, EventRef _event, void *_user_data)
 {
     if( !_event || !_user_data )
         return noErr;
 
-    const auto menu_item = (__bridge FilterPopUpMenuItem*)_user_data;
+    const auto menu_item = (__bridge FilterPopUpMenuItem *)_user_data;
     if( ![menu_item processInterceptedEvent:_event] )
-        return CallNextEventHandler( _handler, _event );
+        return CallNextEventHandler(_handler, _event);
     else
         return noErr;
 }
 
-static constexpr std::array<bool, 256> g_PassthruTable = []{
+static constexpr std::array<bool, 256> g_PassthruTable = [] {
     std::array<bool, 256> table;
     std::fill(table.begin(), table.end(), false);
     table[115] = true; // Home
@@ -217,20 +210,20 @@ static constexpr std::array<bool, 256> g_PassthruTable = []{
     table[124] = true; // Right
     table[125] = true; // Down
     table[126] = true; // Up
-    table[49 ] = true; // Space
-    table[36 ] = true; // Return
-    table[53 ] = true; // Esc
-    table[71 ] = true; // Clear
-    table[76 ] = true; // Insert
-    table[48 ] = true; // Tab
+    table[49] = true;  // Space
+    table[36] = true;  // Return
+    table[53] = true;  // Esc
+    table[71] = true;  // Clear
+    table[76] = true;  // Insert
+    table[48] = true;  // Tab
     table[114] = true; // Help
     table[122] = true; // F1
     table[120] = true; // F2
-    table[99 ] = true; // F3
+    table[99] = true;  // F3
     table[118] = true; // F4
-    table[96 ] = true; // F5
-    table[97 ] = true; // F6
-    table[98 ] = true; // F7
+    table[96] = true;  // F5
+    table[97] = true;  // F6
+    table[98] = true;  // F7
     table[100] = true; // F8
     table[101] = true; // F9
     table[109] = true; // F10
@@ -240,40 +233,40 @@ static constexpr std::array<bool, 256> g_PassthruTable = []{
     table[107] = true; // F14
     table[113] = true; // F15
     table[106] = true; // F16
-    table[64 ] = true; // F17
-    table[79 ] = true; // F18
-    table[80 ] = true; // F19
+    table[64] = true;  // F17
+    table[79] = true;  // F18
+    table[80] = true;  // F19
     return table;
 }();
 
-- (bool) processInterceptedEvent:(EventRef)_event
+- (bool)processInterceptedEvent:(EventRef)_event
 {
     const auto first_responder = self.window.firstResponder;
     if( first_responder == m_Query || first_responder == m_Query.currentEditor )
         return false;
-    
+
     const auto ev = [NSEvent eventWithEventRef:_event];
     if( !ev )
         return false;
-    
+
     if( ev.type != NSEventTypeKeyDown )
         return false;
 
     const auto kc = ev.keyCode;
     if( kc >= g_PassthruTable.size() || g_PassthruTable[kc] )
         return false;
-    
+
     const auto mod_flags = ev.modifierFlags;
     if( (mod_flags & NSEventModifierFlagCommand) != 0 ||
         (mod_flags & NSEventModifierFlagControl) != 0 ||
-        (mod_flags & NSEventModifierFlagOption)  != 0  )
+        (mod_flags & NSEventModifierFlagOption) != 0 )
         return false;
-    
+
     const auto query = m_Query.stringValue;
-    
+
     if( kc == 51 ) { // backspace
         if( query.length > 0 )
-            [self setQuery:[query substringToIndex:query.length-1]];
+            [self setQuery:[query substringToIndex:query.length - 1]];
         return true;
     }
 
@@ -286,11 +279,11 @@ static constexpr std::array<bool, 256> g_PassthruTable = []{
     return false;
 }
 
-- (void) setQuery:(NSString*)_query
+- (void)setQuery:(NSString *)_query
 {
     if( [_query isEqualToString:m_Query.stringValue] )
         return;
- 
+
     m_Query.stringValue = _query;
     [NSRunLoop.currentRunLoop performSelector:@selector(fireNotification)
                                        target:self
@@ -300,35 +293,31 @@ static constexpr std::array<bool, 256> g_PassthruTable = []{
     [self updateVisibility];
 }
 
-- (void) viewDidMoveToWindow
+- (void)viewDidMoveToWindow
 {
     const auto window = self.window;
     if( window ) {
         const auto window_class = window.className;
         const auto is_supported = [window_class isEqualToString:@"NSCarbonMenuWindow"] ||
-            [window_class isEqualToString:@"NSMenuWindowManagerWindow"];
+                                  [window_class isEqualToString:@"NSMenuWindowManagerWindow"];
         if( is_supported == false ) {
             NSLog(@"Sorry, but FilterPopUpMenu doesn't support this version of macOS");
             return;
         }
-        
+
         const auto dispatcher = GetEventDispatcherTarget();
         if( !dispatcher ) {
             NSLog(@"GetEventDispatcherTarget() failed");
             return;
         }
-        
+
         EventTypeSpec evts[2];
         evts[0].eventClass = kEventClassKeyboard;
         evts[0].eventKind = kEventRawKeyDown;
         evts[1].eventClass = kEventClassKeyboard;
         evts[1].eventKind = kEventRawKeyRepeat;
-        const auto result = InstallEventHandler(dispatcher,
-                                                CarbonCallback,
-                                                2,
-                                                &evts[0],
-                                                (__bridge void*)self,
-                                                &m_EventHandler);
+        const auto result = InstallEventHandler(
+            dispatcher, CarbonCallback, 2, &evts[0], (__bridge void *)self, &m_EventHandler);
         if( result != noErr ) {
             NSLog(@"InstallEventHandler() failed");
         }
@@ -349,20 +338,20 @@ static constexpr std::array<bool, 256> g_PassthruTable = []{
                 [filter_menu updateFilter:m_Query.stringValue];
 }
 
-- (void) updateVisibility
+- (void)updateVisibility
 {
     m_Query.hidden = m_Query.stringValue.length == 0;
 }
 
-- (void)controlTextDidChange:(NSNotification *)[[maybe_unused]]_obj
+- (void)controlTextDidChange:(NSNotification *) [[maybe_unused]] _obj
 {
     [self updateVisibility];
     [self fireNotification];
 }
 
-- (BOOL)control:(NSControl *)[[maybe_unused]]_control
-       textView:(NSTextView *)[[maybe_unused]]_textView
-doCommandBySelector:(SEL)commandSelector
+- (BOOL)control:(NSControl *) [[maybe_unused]] _control
+               textView:(NSTextView *) [[maybe_unused]] _textView
+    doCommandBySelector:(SEL)commandSelector
 {
     if( commandSelector == @selector(insertTab:) ) {
         [self.window makeFirstResponder:self.window];

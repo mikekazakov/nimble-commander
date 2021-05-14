38135c70d3280c5c538729a4af048bfd5859f1af
Michael Kazakov <mike.kazakov@gmail.com>
2021-05-14 21:53:58 +0100

    - support for "--help"

---
+++ NimbleCommander/Bootstrap/Usage.h
@@ -0,0 +1,8 @@
+// Copyright (C) 2021 Michael Kazakov. Subject to GNU General Public License version 3.
+#pragma once
+
+namespace nc::bootstrap {
+
+void ProcessCLIUsage(int argc, char *argv[]);
+
+}
--- NimbleCommander/Bootstrap/main.cpp
+++ NimbleCommander/Bootstrap/main.cpp
@@ -1,7 +1,10 @@
 // Copyright (C) 2013-2021 Michael Kazakov. Subject to GNU General Public License version 3.
+#include "Usage.h"
+
 extern "C" int NSApplicationMain(int argc, const char *argv[]);
 
 int main(int argc, char *argv[])
 {
+    nc::bootstrap::ProcessCLIUsage(argc, argv);
     return NSApplicationMain(argc, const_cast<const char **>(argv));
 }

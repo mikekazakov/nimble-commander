fb61be0ee7890e0db01527de7571fbf253308ac7
Michael Kazakov <mike.kazakov@gmail.com>
2021-11-21 21:46:27 +0000

    build fix

--- NimbleCommander/States/FilePanels/Brief/TextWidthsCache.h
+++ NimbleCommander/States/FilePanels/Brief/TextWidthsCache.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2017-2018 Michael Kazakov. Subject to GNU General Public License version 3.
 #pragma once
 
+#include <Cocoa/Cocoa.h>
 #include <Habanero/CFString.h>
 #include <unordered_map>
 #include <string>

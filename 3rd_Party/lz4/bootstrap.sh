f2fb7387213a9af667dab03d33d7fabc9c4d80b5
Michael Kazakov <mike.kazakov@gmail.com>
2022-02-03 22:11:54 +0000

    added lz4

---
+++ 3rd_Party/lz4/bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -o pipefail
+set -o xtrace
+set -e
+
+CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+TMP_DIR=${CUR_DIR}/lz4.tmp
+
+mkdir ${TMP_DIR}
+cd ${TMP_DIR} 
+
+git clone -b v1.9.3 --single-branch https://github.com/lz4/lz4.git
+
+cd lz4/build/cmake
+mkdir builddir
+cd builddir
+
+cmake \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -D CMAKE_OSX_DEPLOYMENT_TARGET="10.15" \
+  -D CMAKE_C_FLAGS="-fvisibility=hidden -flto" \
+  -D BUILD_SHARED_LIBS="OFF" \
+  -D BUILD_STATIC_LIBS="ON" \
+  ..
+make -j
+
+cd ./../../../../../
+rm -rf ./include/
+rm -rf ./lib/
+
+mkdir include
+mkdir lib
+
+cp ./lz4.tmp/lz4/lib/lz4*.h ./include/
+cp ./lz4.tmp/lz4/build/cmake/builddir/liblz4.a ./lib/
+
+rm -rf ${TMP_DIR} 

b665c885c798628cc2e2c850b35c02bd85ea028d
Michael Kazakov <mike.kazakov@gmail.com>
2022-05-28 15:18:37 +0100

    setting an app-wide locale with UTF-8 collation if it wasn't already set

--- NimbleCommander/Bootstrap/main.cpp
+++ NimbleCommander/Bootstrap/main.cpp
@@ -1,10 +1,24 @@
-// Copyright (C) 2013-2021 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2013-2022 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "CLI.h"
+#include <Habanero/CFPtr.h>
+#include <string_view>
 
 extern "C" int NSApplicationMain(int argc, const char *argv[]);
+
+static void SetLocale()
+{
+    const auto current_locale = nc::base::CFPtr<CFLocaleRef>::adopt(CFLocaleCopyCurrent());
+    const CFStringRef cf_ident = CFLocaleGetIdentifier(current_locale.get());
+    char buf[256] = "en_US";
+    CFStringGetCString(cf_ident, buf, sizeof(buf) - 1, kCFStringEncodingUTF8);
+    strcat(buf, ".UTF-8");
+    setlocale(LC_ALL, buf);
+}
 
 int main(int argc, char *argv[])
 {
     nc::bootstrap::ProcessCLIUsage(argc, argv);
+    if( !std::string_view(setlocale(LC_COLLATE, nullptr)).contains("UTF-8") )
+        SetLocale();
     return NSApplicationMain(argc, const_cast<const char **>(argv));
 }

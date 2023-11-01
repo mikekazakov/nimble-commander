40868108dca87a83695238f5e0f6c65c66b1a4e9
Michael Kazakov <mike.kazakov@gmail.com>
2023-11-01 21:12:01 +0000

    Fixed a startup locale issue

--- Habanero/source/SysLocale.cpp
+++ Habanero/source/SysLocale.cpp
@@ -1,6 +1,8 @@
 // Copyright (C) 2023 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "SysLocale.h"
 #include <locale.h>
+#include <locale.h>
+#include <fmt/format.h>
 #include <CoreFoundation/CoreFoundation.h>
 
 namespace nc::base {
@@ -8,20 +10,31 @@ namespace nc::base {
 void SetSystemLocaleAsCLocale() noexcept
 {
     CFLocaleRef loc = CFLocaleCopyCurrent();
+    assert(loc);
     if( loc == nullptr )
         abort();
-    CFStringRef ident = CFLocaleGetIdentifier(loc);
-    if( ident == nullptr )
+
+    // NB! do not use CFLocaleGetIdentifier(), as it might contain additional information, e.g. "en_US@rg=gbzzzz"
+
+    CFTypeRef language_cf = CFLocaleGetValue(loc, kCFLocaleLanguageCode);
+    assert(language_cf);
+    char language_c[256];
+    if( !CFStringGetCString(
+            static_cast<CFStringRef>(language_cf), language_c, sizeof(language_c) - 1, kCFStringEncodingUTF8) )
         abort();
-    char locale[256];
-    if( auto l = CFStringGetCStringPtr(ident, kCFStringEncodingUTF8) )
-        strcpy(locale, l);
-    else
-        CFStringGetCString(ident, locale, sizeof(locale) - 1, kCFStringEncodingUTF8);
-    CFRelease(loc);
-    strcat(locale, ".UTF-8");
-    if( setlocale(LC_ALL, locale) == nullptr )
+
+    CFTypeRef country_cf = CFLocaleGetValue(loc, kCFLocaleCountryCode);
+    assert(country_cf);
+    char country_c[256];
+    if( !CFStringGetCString(
+            static_cast<CFStringRef>(country_cf), country_c, sizeof(country_c) - 1, kCFStringEncodingUTF8) )
         abort();
+
+    const std::string locale = fmt::format("{}_{}.UTF-8", language_c, country_c);
+    if( setlocale(LC_ALL, locale.c_str()) == nullptr ) {
+        fmt::print(stderr, "failed to set C locale to '{}', aborting\n", locale);
+        abort();
+    }
 }
 
 } // namespace nc::base

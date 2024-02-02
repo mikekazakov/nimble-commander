6949d6cad1dac6ed7be9a90aa6447a9e12068b86
Michael Kazakov <mike.kazakov@gmail.com>
2024-02-02 18:51:20 +0000

    Cleaned up the build script

--- Scripts/build_nightly.sh
+++ Scripts/build_nightly.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -x
 set -e
 set -o pipefail
 
@@ -17,24 +16,19 @@ fi
 # https://github.com/xcpretty/xcpretty/issues/48
 export LC_CTYPE=en_US.UTF-8
 
-# get current directory
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# allocate a dir for build artifacts
-BUILD_DIR="${SCRIPTS_DIR}/build_nightly.tmp"
-mkdir -p "${BUILD_DIR}"
-
-# all builds paths will be relative to ROOT_DIR
-ROOT_DIR=$(cd "$SCRIPTS_DIR/.." && pwd)
-
-XCODEPROJ="../Source/NimbleCommander/NimbleCommander.xcodeproj"
-ARCHIVE_PATH="${BUILD_DIR}/NC_NonMAS.xcarchive"
-BUILT_PATH="${BUILD_DIR}/built"
-
-mkdir -p "${ARCHIVE_PATH}"
-
 PBUDDY=/usr/libexec/PlistBuddy
 
+# Set up the paths to the sources and artifacts
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR=$(cd "$SCRIPTS_DIR/.." && pwd)
+XCODEPROJ="${ROOT_DIR}/Source/NimbleCommander/NimbleCommander.xcodeproj"
+BUILD_DIR="${SCRIPTS_DIR}/build_nightly.tmp"
+ARCHIVE_PATH="${BUILD_DIR}/NC_NonMAS.xcarchive"
+BUILT_PATH="${BUILD_DIR}/built"
+mkdir -p "${BUILD_DIR}"
+mkdir -p "${ARCHIVE_PATH}"
+
+# Enable caching with ccache if it's available in the system
 if type -p /usr/local/bin/ccache >/dev/null 2>&1; then
     echo Using ccache
     export CCACHE_BASEDIR="${ROOT_DIR}"
@@ -43,15 +37,14 @@ if type -p /usr/local/bin/ccache >/dev/null 2>&1; then
     export CXX="${SCRIPTS_DIR}/ccache-clang++"
 fi
 
+# Gather common flags in the XC variable
 XC="xcodebuild \
  -project ${XCODEPROJ} \
  -scheme NimbleCommander-NonMAS \
  -configuration Release \
  OTHER_CFLAGS=\"-fdebug-prefix-map=${ROOT_DIR}=.\""
 
-APP_NAME=$($XC -showBuildSettings | grep " FULL_PRODUCT_NAME =" | sed -e 's/.*= *//' )
-APP_PATH="${BUILT_PATH}/${APP_NAME}"
-
+# Build and archive the project
 $XC -archivePath ${ARCHIVE_PATH} archive | xcpretty
 
 # Export a signed version
@@ -61,10 +54,13 @@ xcodebuild -exportArchive \
  -exportOptionsPlist export_options.plist
 
 # Extract the version number and the build number
+APP_NAME=$($XC -showBuildSettings | grep " FULL_PRODUCT_NAME =" | sed -e 's/.*= *//' )
+APP_PATH="${BUILT_PATH}/${APP_NAME}"
 VERSION=$( $PBUDDY -c "Print CFBundleShortVersionString" "${APP_PATH}/Contents/Info.plist" )
 BUILD=$( $PBUDDY -c "Print CFBundleVersion" "${APP_PATH}/Contents/Info.plist" )
 DMG_NAME="nimble-commander-nightly-${VERSION}(${BUILD}).dmg"
 
+# Wrap the built application into a .dmg disk image
 create-dmg \
  --volname "Nimble Commander Nightly" \
  --window-pos 200 200 \
@@ -78,8 +74,10 @@ create-dmg \
  "${DMG_NAME}" \
  "${APP_PATH}"
 
-# Upload the built dmg into Apple's notary service
+# Upload the built dmg into Apple's notary service and wait for the response
 xcrun notarytool submit ${DMG_NAME} --keychain-profile AC_PASSWORD --wait
 
 # Finally, staple the dmg
 xcrun stapler staple "${DMG_NAME}"
+
+# Done!

64d75b699856469571719e23ec1893bfefd2135a
Michael Kazakov <mike.kazakov@gmail.com>
2024-04-10 16:10:43 +0100

    Added PanelListViewColumns::Tags, simplified the code

--- Source/NimbleCommander/NimbleCommander/States/FilePanels/List/Layout.h
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/List/Layout.h
@@ -1,12 +1,11 @@
-// Copyright (C) 2016-2021 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2016-2024 Michael Kazakov. Subject to GNU General Public License version 3.
 #pragma once
 #include <boost/container/static_vector.hpp>
 
 namespace nc::panel {
 
-// these values are used in serializaition, don't change existing.
-enum class PanelListViewColumns : signed char
-{
+// these values are used in serialization, don't change the existing identifiers.
+enum class PanelListViewColumns : signed char {
     Empty = 0,
     Filename = 1,
     Extension = 7,
@@ -14,26 +13,25 @@ enum class PanelListViewColumns : signed char
     DateCreated = 3,
     DateAdded = 4,
     DateModified = 5,
-    DateAccessed = 6
+    DateAccessed = 6,
+    Tags = 8
 };
 
 struct PanelListViewColumnsLayout {
     struct Column {
-        PanelListViewColumns kind; // = PanelListViewColumns::Empty;
-        short width;               // = -1
-        short max_width;           // = -1
-        short min_width;           // = -1
-        Column() noexcept;
-        bool operator==(const Column &_rhs) const noexcept;
-        bool operator!=(const Column &_rhs) const noexcept;
+        PanelListViewColumns kind = PanelListViewColumns::Empty;
+        short width = -1;
+        short max_width = -1;
+        short min_width = -1;
+        bool operator==(const Column &_rhs) const noexcept = default;
+        bool operator!=(const Column &_rhs) const noexcept = default;
     };
 
-    boost::container::static_vector<Column, 7> columns;
-    unsigned char icon_scale; // = 1
+    boost::container::static_vector<Column, 8> columns;
+    unsigned char icon_scale = 1;
 
-    PanelListViewColumnsLayout() noexcept;
-    bool operator==(const PanelListViewColumnsLayout &_rhs) const noexcept;
-    bool operator!=(const PanelListViewColumnsLayout &_rhs) const noexcept;
+    bool operator==(const PanelListViewColumnsLayout &_rhs) const noexcept = default;
+    bool operator!=(const PanelListViewColumnsLayout &_rhs) const noexcept = default;
 };
 
 } // namespace nc::panel
--- Source/NimbleCommander/NimbleCommander/States/FilePanels/List/Layout.mm
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/List/Layout.mm
@@ -1,38 +1,8 @@
-// Copyright (C) 2016-2021 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2016-2024 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "Layout.h"
 
 namespace nc::panel {
 
 static_assert(sizeof(PanelListViewColumnsLayout::Column) == 8);
-
-PanelListViewColumnsLayout::Column::Column() noexcept
-    : kind(PanelListViewColumns::Empty), width(-1), max_width(-1), min_width(-1)
-{
-}
-
-bool PanelListViewColumnsLayout::Column::operator==(const Column &_rhs) const noexcept
-{
-    return kind == _rhs.kind && width == _rhs.width && max_width == _rhs.max_width &&
-           min_width == _rhs.min_width;
-}
-
-bool PanelListViewColumnsLayout::Column::operator!=(const Column &_rhs) const noexcept
-{
-    return !(*this == _rhs);
-}
-
-PanelListViewColumnsLayout::PanelListViewColumnsLayout() noexcept : icon_scale(1)
-{
-}
-
-bool PanelListViewColumnsLayout::operator==(const PanelListViewColumnsLayout &_rhs) const noexcept
-{
-    return columns == _rhs.columns && icon_scale == _rhs.icon_scale;
-}
-
-bool PanelListViewColumnsLayout::operator!=(const PanelListViewColumnsLayout &_rhs) const noexcept
-{
-    return !(*this == _rhs);
-}
 
 } // namespace nc::panel

04868181846e52ff0ff7ea5cf9d080898e4dbbe1
Michael G. Kazakov <mike.kazakov@gmail.com>
2024-04-14 17:30:47 +0100

    Migrated to SecItem, fixed #178 (#193)

--- Source/Utility/source/KeychainServices.cpp
+++ Source/Utility/source/KeychainServices.cpp
@@ -1,11 +1,9 @@
-// Copyright (C) 2014-2022 Michael Kazakov. Subject to GNU General Public License version 3.
-#include <Security/Security.h>
+// Copyright (C) 2014-2024 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "../include/Utility/KeychainServices.h"
-#pragma clang diagnostic ignored "-Wdeprecated-declarations" // See issue #147
+#include <Security/Security.h>
+#include <Base/CFPtr.h>
 
-KeychainServices::KeychainServices()
-{
-}
+KeychainServices::KeychainServices() = default;
 
 KeychainServices &KeychainServices::Instance()
 {
@@ -13,76 +11,73 @@ KeychainServices &KeychainServices::Instance()
     return *inst;
 }
 
-bool KeychainServices::SetPassword(const std::string &_where,
-                                   const std::string &_account,
-                                   const std::string &_password)
+bool KeychainServices::SetPassword(const std::string &_where, const std::string &_account, const std::string &_password)
 {
-    OSStatus result = SecKeychainAddInternetPassword(nullptr,
-                                                     UInt32(_where.length()),
-                                                     _where.c_str(),
-                                                     0,
-                                                     nullptr,
-                                                     UInt32(_account.length()),
-                                                     _account.c_str(),
-                                                     0,
-                                                     nullptr,
-                                                     0,
-                                                     kSecProtocolTypeAny,
-                                                     kSecAuthenticationTypeDefault,
-                                                     UInt32(_password.length()),
-                                                     _password.c_str(),
-                                                     nullptr);
-    if( result == errSecDuplicateItem ) {
-        SecKeychainItemRef item;
-        result = SecKeychainFindInternetPassword(nullptr,
-                                                 UInt32(_where.length()),
-                                                 _where.c_str(),
-                                                 0,
-                                                 nullptr,
-                                                 UInt32(_account.length()),
-                                                 _account.c_str(),
-                                                 0,
-                                                 nullptr,
-                                                 0,
-                                                 kSecProtocolTypeAny,
-                                                 kSecAuthenticationTypeDefault,
-                                                 nullptr,
-                                                 nullptr,
-                                                 &item);
-        if( result == 0 ) {
-            result = SecKeychainItemModifyAttributesAndData(
-                item, nullptr, UInt32(_password.length()), _password.c_str());
-            CFRelease(item);
-        }
+    using nc::base::CFPtr;
+    const CFPtr<CFMutableDictionaryRef> query =
+        CFPtr<CFMutableDictionaryRef>::adopt(CFDictionaryCreateMutable(nullptr,                        //
+                                                                       0,                              //
+                                                                       &kCFTypeDictionaryKeyCallBacks, //
+                                                                       &kCFTypeDictionaryValueCallBacks));
+    CFDictionarySetValue(query.get(), kSecClass, kSecClassInternetPassword);
+    const CFPtr<CFStringRef> server =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _where.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrServer, server.get());
+    const CFPtr<CFStringRef> account =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _account.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrAccount, account.get());
+    CFDictionarySetValue(query.get(), kSecMatchLimit, kSecMatchLimitOne);
+    CFDictionarySetValue(query.get(), kSecReturnAttributes, kCFBooleanTrue);
+    const CFPtr<CFDataRef> value = CFPtr<CFDataRef>::adopt(
+        CFDataCreate(nullptr, reinterpret_cast<const UInt8 *>(_password.c_str()), _password.length()));
+
+    CFDictionaryRef existing = nullptr;
+    const OSStatus status = SecItemCopyMatching(query.get(), reinterpret_cast<CFTypeRef *>(&existing));
+
+    if( existing ) {
+        CFRelease(existing);
     }
 
-    return result == 0;
+    if( status == errSecSuccess ) {
+        // update existing item
+        CFPtr<CFMutableDictionaryRef> update = CFPtr<CFMutableDictionaryRef>::adopt(
+            CFDictionaryCreateMutable(nullptr, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks));
+        CFDictionarySetValue(update.get(), kSecValueData, value.get());
+        return SecItemUpdate(query.get(), update.get()) == errSecSuccess;
+    }
+    else if( status == errSecItemNotFound ) {
+        // add new item
+        CFDictionarySetValue(query.get(), kSecValueData, value.get());
+        return SecItemAdd(query.get(), nullptr) == errSecSuccess;
+    }
+    else {
+        return false;
+    }
 }
 
-bool KeychainServices::GetPassword(const std::string &_where,
-                                   const std::string &_account,
-                                   std::string &_password)
+bool KeychainServices::GetPassword(const std::string &_where, const std::string &_account, std::string &_password)
 {
-    UInt32 len = 0;
-    void *data = 0;
-    OSStatus result = SecKeychainFindInternetPassword(nullptr,
-                                                      UInt32(_where.length()),
-                                                      _where.c_str(),
-                                                      0,
-                                                      nullptr,
-                                                      UInt32(_account.length()),
-                                                      _account.c_str(),
-                                                      0,
-                                                      nullptr,
-                                                      0,
-                                                      kSecProtocolTypeAny,
-                                                      kSecAuthenticationTypeDefault,
-                                                      &len,
-                                                      &data,
-                                                      nullptr);
-    if( result == 0 ) {
-        _password.assign(static_cast<char *>(data), len);
-        SecKeychainItemFreeContent(nullptr, data);
+    using nc::base::CFPtr;
+    CFPtr<CFMutableDictionaryRef> query =
+        CFPtr<CFMutableDictionaryRef>::adopt(CFDictionaryCreateMutable(nullptr,                        //
+                                                                       0,                              //
+                                                                       &kCFTypeDictionaryKeyCallBacks, //
+                                                                       &kCFTypeDictionaryValueCallBacks));
+    CFDictionarySetValue(query.get(), kSecClass, kSecClassInternetPassword);
+    CFPtr<CFStringRef> server =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _where.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrServer, server.get());
+    CFPtr<CFStringRef> account =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _account.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrAccount, account.get());
+    CFDictionarySetValue(query.get(), kSecReturnData, kCFBooleanTrue);
+    CFDictionarySetValue(query.get(), kSecMatchLimit, kSecMatchLimitOne);
+
+    CFDataRef out = nullptr;
+    const OSStatus status = SecItemCopyMatching(query.get(), reinterpret_cast<CFTypeRef *>(&out));
+    CFPtr<CFDataRef> result = CFPtr<CFDataRef>::adopt(out);
+    if( status == errSecSuccess && result ) {
+        _password.assign(reinterpret_cast<const char *>(CFDataGetBytePtr(result.get())), CFDataGetLength(result.get()));
         return true;
     }
     return false;
@@ -90,25 +85,18 @@ bool KeychainServices::GetPassword(const std::string &_where,
 
 bool KeychainServices::ErasePassword(const std::string &_where, const std::string &_account)
 {
-    SecKeychainItemRef item;
-    OSStatus result = SecKeychainFindInternetPassword(nullptr,
-                                                      UInt32(_where.length()),
-                                                      _where.c_str(),
-                                                      0,
-                                                      nullptr,
-                                                      UInt32(_account.length()),
-                                                      _account.c_str(),
-                                                      0,
-                                                      nullptr,
-                                                      0,
-                                                      kSecProtocolTypeAny,
-                                                      kSecAuthenticationTypeDefault,
-                                                      nullptr,
-                                                      nullptr,
-                                                      &item);
-    if( result == 0 ) {
-        result = SecKeychainItemDelete(item);
-        CFRelease(item);
-    }
-    return result == 0;
+    using nc::base::CFPtr;
+    CFPtr<CFMutableDictionaryRef> query =
+        CFPtr<CFMutableDictionaryRef>::adopt(CFDictionaryCreateMutable(nullptr,                        //
+                                                                       0,                              //
+                                                                       &kCFTypeDictionaryKeyCallBacks, //
+                                                                       &kCFTypeDictionaryValueCallBacks));
+    CFDictionarySetValue(query.get(), kSecClass, kSecClassInternetPassword);
+    CFPtr<CFStringRef> server =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _where.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrServer, server.get());
+    CFPtr<CFStringRef> account =
+        CFPtr<CFStringRef>::adopt(CFStringCreateWithCString(nullptr, _account.c_str(), kCFStringEncodingUTF8));
+    CFDictionarySetValue(query.get(), kSecAttrAccount, account.get());
+    return SecItemDelete(query.get()) == errSecSuccess;
 }

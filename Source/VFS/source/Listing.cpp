ad7f5ca03a5c6bbbb1b5531a005ef59ca710f0fe
Michael Kazakov <mike.kazakov@gmail.com>
2024-05-05 21:43:00 +0100

    Enabling more clang-tidy checks

--- Source/VFS/source/Listing.cpp
+++ Source/VFS/source/Listing.cpp
@@ -10,8 +10,8 @@ namespace nc::vfs {
 using nc::base::variable_container;
 
 // static_assert(sizeof(Listing) <= 824); // became 944 on Xcode15 ???
-static_assert(std::is_move_constructible<ListingItem>::value);
-static_assert(std::is_move_constructible<Listing::iterator>::value);
+static_assert(std::is_move_constructible_v<ListingItem>);
+static_assert(std::is_move_constructible_v<Listing::iterator>);
 
 static bool BasicDirectoryCheck(std::string_view _str) noexcept
 {

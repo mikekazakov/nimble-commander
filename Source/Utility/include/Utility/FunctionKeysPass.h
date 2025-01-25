ae3a8ea326ecaab1235b845f9b6ca1ac9d0d23bd
Michael G. Kazakov <mike.kazakov@gmail.com>
2025-01-25 15:04:13 +0000

    Added more logging to FunctionalKeysPass (#515)

--- Source/Utility/include/Utility/FunctionKeysPass.h
+++ Source/Utility/include/Utility/FunctionKeysPass.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2016-2022 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2016-2025 Michael Kazakov. Subject to GNU General Public License version 3.
 #pragma once
 
 #include <CoreGraphics/CGEventTypes.h>
@@ -20,11 +20,14 @@ public:
 
 private:
     static bool ObtainAccessiblityRights();
-    static CGEventRef HandleRegularKeyEvents(CGEventType _type, CGEventRef _event);
-    static CGEventRef HandleControlButtons(CGEventType _type, CGEventRef _event);
-    CGEventRef Callback(CGEventTapProxy _proxy, CGEventType _type, CGEventRef _event);
-    CFMachPortRef m_Port;
-    bool m_Enabled;
+    static CGEventRef HandleRegularKeyEvents(CGEventType _type, CGEventRef _event) noexcept;
+    static CGEventRef HandleControlButtons(CGEventType _type, CGEventRef _event) noexcept;
+    CGEventRef Callback(CGEventTapProxy _proxy, CGEventType _type, CGEventRef _event) noexcept;
+    void OnDidBecomeActive() noexcept;
+    void OnResignActive() noexcept;
+
+    CFMachPortRef m_Port = nullptr;
+    bool m_Enabled = false;
 };
 
 } // namespace nc::utility

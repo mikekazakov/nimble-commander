1dcdd8d6008e6430978f641d1e5ef6cc838c3e19
Michael Kazakov <mike.kazakov@gmail.com>
2025-11-30 13:14:52 +0000

    Added the "All" scheme, redone run_clang_tidy to use it, instead of building targets one by one

--- Scripts/run_clang_tidy.sh
+++ Scripts/run_clang_tidy.sh
@@ -21,6 +21,9 @@ fi
 # https://github.com/xcpretty/xcpretty/issues/48
 export LC_CTYPE=en_US.UTF-8
 
+# Determine the host architecture
+HOST_ARCH=$(uname -m)
+
 # Tools from LLVM
 RUNTIDY=/opt/homebrew/opt/llvm/bin/run-clang-tidy
 TIDY=/opt/homebrew/opt/llvm/bin/clang-tidy
@@ -45,45 +48,28 @@ xattr -w com.apple.xcode.CreatedByBuildSystem true "${BUILD_DIR}"
 # A project to build
 XCODEPROJ="${ROOT_DIR}/Source/NimbleCommander/NimbleCommander.xcodeproj"
 
-# Compose a list of targets to build
-uts=$(xcodebuild -project ${XCODEPROJ} -list | awk -v word="Schemes:" 'BEGIN {found=0} found {if ($0 ~ /UT$/) print} $0 ~ word {found=1}' | sed 's/^[[:space:]]*//')
-its=$(xcodebuild -project ${XCODEPROJ} -list | awk -v word="Schemes:" 'BEGIN {found=0} found {if ($0 ~ /IT$/) print} $0 ~ word {found=1}' | sed 's/^[[:space:]]*//')
-others=("NimbleCommander-Unsigned" "RoutedIO" "info.filesmanager.Files.PrivilegedIOHelperV2" "Highlighter" )
-targets=("${uts[@]}" "${its[@]}" "${others[@]}")
-echo Building these targets: ${targets[@]}
+echo Building the target "All"
 
 # Common invocation of xcodebuild
 XC="xcodebuild \
  -project ${XCODEPROJ} \
+ -scheme All \
  -configuration Debug \
+ -destination "platform=macOS,arch=${HOST_ARCH}" \
  SYMROOT=${BUILD_DIR} \
  OBJROOT=${BUILD_DIR} \
+ -parallelizeTargets \
  ONLY_ACTIVE_ARCH=YES \
  COMPILER_INDEX_STORE_ENABLE=NO"
 
-# Clean each target
-for target in ${targets[@]}; do
-    $XC -scheme ${target} clean
-done
+# Clean everything first
+${XC} clean
 
-# Build each target
-echo "[]" > compile_commands.json
-for target in ${targets[@]}; do
-    # Build the target and capture the invocation command
-    $XC -scheme ${target} build | xcpretty -r json-compilation-database --output compile_commands-${target}.json
+# Build the target and capture the invocation command
+$XC build | xcpretty -r json-compilation-database --output compile_commands.json
 
-    # Remove the "-ivfsstatcache path" flags that clang-tidy doesn't understand
-    sed -i '' 's/-ivfsstatcache[^\"]*\.sdkstatcache//g' "compile_commands-${target}.json"
-    
-    # Temporarily combine the new JSON with the accumulating compile_commands.json
-    jq -s 'add' compile_commands.json "compile_commands-${target}.json" > temp_compile_commands.json
-
-    # Replace the main compile_commands.json with the updated version
-    mv temp_compile_commands.json compile_commands.json
-    
-    # The compile commands of this target
-    rm "compile_commands-${target}.json"
-done
+# Remove the "-ivfsstatcache path" flags that clang-tidy doesn't understand
+sed -i '' 's/-ivfsstatcache[^\"]*\.sdkstatcache//g' "compile_commands.json"
 
 # Extract all unique response files into an array, removing the '@' symbol
 response_files=($(jq -r '.[].command' "compile_commands.json" | grep -o '@[^ ]*' | sed 's/^@//' | sort | uniq))

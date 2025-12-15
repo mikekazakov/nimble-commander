096f68203c7b14d40d56d943e9eb11e0f458a2ec
Michael Kazakov <mike.kazakov@gmail.com>
2025-12-15 16:11:28 +0000

    Moved calls to [NSWorkspace iconForFile] into a background queue

--- Source/NimbleCommander/NimbleCommander/States/FilePanels/Gallery/PanelGalleryCentralView.mm
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/Gallery/PanelGalleryCentralView.mm
@@ -5,13 +5,19 @@
 #include <Utility/StringExtras.h>
 #include <Utility/PathManip.h>
 #include <Base/dispatch_cpp.h>
+#include <Base/mach_time.h>
 #include <Quartz/Quartz.h>
+
+static const std::chrono::nanoseconds g_DebounceDelay = std::chrono::milliseconds{16};
 
 @implementation NCPanelGalleryCentralView {
     const nc::utility::UTIDB *m_UTIDB;
     QLPreviewView *m_QLView;
     NSImageView *m_FallbackImageView;
-    std::filesystem::path m_FallbackImagePath; // ??
+    std::atomic_ullong m_CurrentTicket;
+
+    std::filesystem::path m_CurrentPath;
+    VFSHostWeakPtr m_CurrentHost;
 
     // It's a workaround for the macOS bug reported in FB9809109/FB5352643.
     bool m_CurrentPreviewIsHazardous;
@@ -28,6 +34,7 @@
         return nil;
     m_UTIDB = &_UTIDB;
     m_CurrentPreviewIsHazardous = false;
+    m_CurrentTicket = 0;
 
     self.translatesAutoresizingMaskIntoConstraints = false;
 
@@ -64,39 +71,115 @@
 - (void)showVFSItem:(VFSListingItem)_item
 {
     dispatch_assert_main_queue();
+    if( !_item )
+        return; // ignore bogus requests. TODO: reset the view here?
 
-    // For now only supporting native vfs
-    if( _item && _item.Host()->IsNativeFS() ) {
-        const std::string path = _item.Path();
-        if( NSString *ns_path = [NSString stringWithUTF8StdString:path] ) {
-            if( NSURL *url = [NSURL fileURLWithPath:ns_path] ) {
-                if( [self couldBeSupportedByQuickLook:_item] ) {
-                    m_QLView.hidden = false;
-                    m_FallbackImageView.hidden = true;
-                    m_FallbackImagePath = "";
-                    if( m_CurrentPreviewIsHazardous ) {
-                        m_QLView.previewItem =
-                            nil; // to prevent an ObjC exception from inside QL - reset the view first
-                    }
-                    m_QLView.previewItem = url;
-                }
-                else {
-                    m_QLView.hidden = true;
-                    m_QLView.previewItem =
-                        nil; // NB! Without resetting the preview to nil, it somehow manages to completely freeze
-                    m_FallbackImageView.hidden = false;
+    if( m_CurrentPath == _item.Path() && m_CurrentHost.lock() == _item.Host() )
+        return; // nothing to do - redundant request
 
-                    // TODO: never call this in the main thread
-                    if( m_FallbackImagePath != path ) {
-                        // fmt::println("Gallery fallback image for path: {}", path);
-                        m_FallbackImageView.image = [[NSWorkspace sharedWorkspace] iconForFile:ns_path];
-                        m_FallbackImagePath = path;
-                    }
-                }
-                m_CurrentPreviewIsHazardous = [self isHazardousPath:path];
-            }
+    const bool is_native_vfs = _item.Host()->IsNativeFS();
+    const bool potentially_ql = [self couldBeSupportedByQuickLook:_item];
+
+    // Regardless of the following logic, a new request means cancelling any previous ones
+    ++m_CurrentTicket;
+
+    // Update our state to represent this item
+    m_CurrentHost = _item.Host();
+    m_CurrentPath = _item.Path();
+
+    if( is_native_vfs && potentially_ql ) {
+        // Dispatch this request to QL right away, let it deal with it asynchronously
+        [self previewNativeQL:_item];
+        return;
+    }
+
+    if( is_native_vfs && !potentially_ql ) {
+        [self previewNativeIcon:_item ticket:m_CurrentTicket];
+    }
+}
+
+- (void)previewNativeQL:(const VFSListingItem &)_item
+{
+    dispatch_assert_main_queue();
+    assert(_item);
+    const std::string path = _item.Path();
+    if( NSString *ns_path = [NSString stringWithUTF8StdString:path] ) {
+        if( NSURL *url = [NSURL fileURLWithPath:ns_path] ) {
+            [self ensureQLVisible];
+            if( m_CurrentPreviewIsHazardous )
+                m_QLView.previewItem = nil; // to prevent an ObjC exception from inside QL - reset the view first
+            m_QLView.previewItem = url;
+            m_CurrentPreviewIsHazardous = [self isHazardousPath:path];
         }
     }
+}
+
+- (void)previewNativeIcon:(const VFSListingItem &)_item ticket:(uint64_t)_ticket
+{
+    dispatch_assert_main_queue();
+    assert(_item);
+
+    const NSRect bounds = self.bounds;
+
+    __weak NCPanelGalleryCentralView *const weak_self = self;
+    const std::string path = _item.Path();
+    auto worker = [weak_self, path, _ticket, bounds] {
+        dispatch_assert_background_queue();
+        if( NCPanelGalleryCentralView *const strong_self = weak_self;
+            strong_self == nil || _ticket != strong_self->m_CurrentTicket )
+            return; // Expired request
+
+        NSString *const ns_path = [NSString stringWithUTF8StdString:path];
+        if( ns_path == nil )
+            return;
+
+        // Produce the icon. It can be a (very) lazy NSImage, which is completely opaque and system-dependent.
+        NSImage *const image = [[NSWorkspace sharedWorkspace] iconForFile:ns_path];
+
+        if( NCPanelGalleryCentralView *const strong_self = weak_self;
+            strong_self == nil || _ticket != strong_self->m_CurrentTicket )
+            return; // Expired request
+
+        // Force image rendering at ~ desired size, here at the background, instead of the main thread.
+        // This avoids (or at least reduces) stuttering when the image is finally assigned to the image view.
+        // The result of this operation is purposely discarded.
+        // TODO: how to handle pixel scaling here?
+        NSRect tmp_bounds = bounds;
+        [image CGImageForProposedRect:&tmp_bounds context:nil hints:nil];
+
+        dispatch_to_main_queue([weak_self, image, _ticket] {
+            if( NCPanelGalleryCentralView *const strong_self = weak_self ) {
+                [strong_self commitPreviewedIcon:image ticket:_ticket];
+            }
+        });
+    };
+
+    // Debounce expensive requests by delaying them slightly + relying on ticketing that discards previous requests.
+    dispatch_after(g_DebounceDelay, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), worker);
+}
+
+- (void)commitPreviewedIcon:(NSImage *)_image ticket:(uint64_t)_ticket
+{
+    dispatch_assert_main_queue();
+    if( _ticket != m_CurrentTicket )
+        return; // Expired request
+    [self ensureFallbackVisible];
+    m_FallbackImageView.image = _image;
+}
+
+- (void)ensureQLVisible
+{
+    dispatch_assert_main_queue();
+    m_QLView.hidden = false;
+    m_FallbackImageView.hidden = true;
+}
+
+- (void)ensureFallbackVisible
+{
+    dispatch_assert_main_queue();
+    m_QLView.hidden = true;
+    m_QLView.previewItem = nil; // NB! Without resetting the preview to nil, it somehow manages to completely freeze
+    m_FallbackImageView.hidden = false;
 }
 
 - (bool)couldBeSupportedByQuickLook:(const VFSListingItem &)_item

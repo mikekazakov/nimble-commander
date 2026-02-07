038661fbffc2c79aa5d8938aac68b0b2600e331a
Michael G. Kazakov <mike.kazakov@gmail.com>
2026-02-07 20:19:37 +0000

    Fixed the issue with illegible quick search in Classic theme (#657)
    * - Don't use NSSearchField in the header view, it looks weird with custom coloring.
    - Drive-by - fixed the incorrect vertical alignment of custom font with sizes != 13.
    - Fixed #653
    
    * Apply the same fix to the footer view, which also had troubles with custom fonts

--- Source/NimbleCommander/NimbleCommander/States/FilePanels/PanelViewHeader.h
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/PanelViewHeader.h
@@ -1,29 +1,40 @@
-// Copyright (C) 2016-2020 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2016-2026 Michael Kazakov. Subject to GNU General Public License version 3.
 #pragma once
 
 #include <Panel/PanelDataSortMode.h>
 #include "PanelViewHeaderTheme.h"
 
-@interface NCPanelViewHeader : NSView <NSSearchFieldDelegate>
+@interface NCPanelViewHeader : NSView <NSTextFieldDelegate>
 
 - (id)initWithFrame:(NSRect)frameRect NS_UNAVAILABLE;
 - (id)initWithFrame:(NSRect)frameRect theme:(std::unique_ptr<nc::panel::HeaderTheme>)_theme;
 
+// Updates header title according to the new path. Should be called by controller when path is changed.
 - (void)setPath:(NSString *)_path;
 
+// Progress indicator located in the header. Shown only when displaying activity.
 @property(nonatomic, readonly) NSProgressIndicator *busyIndicator;
+
+// Search field located in the header. Hidden when not searching.
 @property(nonatomic) NSString *searchPrompt;
+
+// Number of matches for the current search query. Should be set by controller when search query is changed.
 @property(nonatomic) int searchMatches;
+
+// Sort mode for the panel. Displayed in a down-down button.
 @property(nonatomic) nc::panel::data::SortMode sortMode;
+
+// Called by the view when sort mode is changed by user via sort mode button in header.
 @property(nonatomic) std::function<void(nc::panel::data::SortMode)> sortModeChangeCallback;
 
-/**
- * Calling with nil means discarding the search via (X) button.
- */
-@property(nonatomic) std::function<void(NSString *)> searchRequestChangeCallback;
+// Called by the view when search query is changed by user via search field in header.
+// When the argument is nil this should be interpreted as discarding the search via (X) or Esc button.
+@property(nonatomic) std::function<void(NSString *_query)> searchRequestChangeCallback;
 
+// Updates the look of the header depending on the parent panel being active or not.
 @property(nonatomic) bool active;
 
+// Used to return the focus when search field is dismissed.
 @property(nonatomic, weak) NSResponder *defaultResponder;
 
 @end

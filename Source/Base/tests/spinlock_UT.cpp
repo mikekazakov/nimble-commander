c4c535ebe2a956d673bc63bc4d0251a06f1ee790
Michael Kazakov <mike.kazakov@gmail.com>
2026-02-07 21:41:11 +0000

    More correct unit test for a spinlock

--- Source/Base/tests/spinlock_UT.cpp
+++ Source/Base/tests/spinlock_UT.cpp
@@ -1,6 +1,7 @@
-// Copyright (C) 2018-2021 Michael Kazakov. Subject to GNU General Public License version 3.
+// Copyright (C) 2018-2026 Michael Kazakov. Subject to GNU General Public License version 3.
 #include "spinlock.h"
 #include "UnitTests_main.h"
+#include <array>
 #include <string>
 #include <thread>
 #include <atomic>
@@ -20,25 +21,29 @@ TEST_CASE(PREFIX "non-contested passage")
 
 TEST_CASE(PREFIX "contested passage")
 {
-    std::atomic_int value = 0;
+    constexpr int num_threads = 8;
+    constexpr int increments_per_thread = 10000;
+
     nc::spinlock lock;
+    int counter = 0;
 
-    auto th1 = std::thread{[&] {
-        auto guard = std::lock_guard{lock};
-        CHECK(value == 0);
-        std::this_thread::sleep_for(std::chrono::milliseconds{10});
-        value = 1;
-    }};
-    std::this_thread::sleep_for(std::chrono::milliseconds{1});
-    auto th2 = std::thread{[&] {
-        auto guard = std::lock_guard{lock};
-        CHECK(value == 1);
-        value = 2;
-    }};
+    auto worker = [&]() {
+        for( int i = 0; i < increments_per_thread; ++i ) {
+            auto guard = std::lock_guard{lock};
+            ++counter;
+        }
+    };
 
-    th1.join();
-    th2.join();
-    CHECK(value == 2);
+    std::array<std::thread, num_threads> threads;
+    for( int i = 0; i < num_threads; ++i ) {
+        threads[i] = std::thread(worker);
+    }
+
+    for( auto &t : threads ) {
+        t.join();
+    }
+
+    CHECK(counter == num_threads * increments_per_thread);
 }
 
 #undef PREFIX

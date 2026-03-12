5bbdb095386e1b574186038f16f78d21056b59ac
Stefano Locati <stivlo@gmail.com>
2026-03-12 16:39:59 +0700

    Add context menu pathname copy alternate

--- Source/NimbleCommander/NimbleCommander/States/FilePanels/Actions/CopyFilePaths.h
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/Actions/CopyFilePaths.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "DefaultAction.h"
+#include <VFS/VFS.h>
 
 namespace nc::panel::actions {
 
@@ -19,5 +20,19 @@ struct CopyFileDirectory final : PanelAction {
     [[nodiscard]] bool Predicate(PanelController *_source) const override;
     void Perform(PanelController *_source, id _sender) const override;
 };
+
+namespace context {
+
+struct CopyPathname final : PanelAction {
+    explicit CopyPathname(const std::vector<VFSListingItem> &_items);
+    [[nodiscard]] bool Predicate(PanelController *_source) const override;
+    [[nodiscard]] bool ValidateMenuItem(PanelController *_source, NSMenuItem *_item) const override;
+    void Perform(PanelController *_source, id _sender) const override;
+
+private:
+    const std::vector<VFSListingItem> &m_Items;
+};
+
+} // namespace context
 
 } // namespace nc::panel::actions
--- Source/NimbleCommander/NimbleCommander/States/FilePanels/Actions/CopyFilePaths.mm
+++ Source/NimbleCommander/NimbleCommander/States/FilePanels/Actions/CopyFilePaths.mm
@@ -4,6 +4,7 @@
 #include "../PanelView.h"
 #include "CopyFilePaths.h"
 #include <VFS/VFS.h>
+#include <functional>
 #include <Utility/StringExtras.h>
 #include <numeric>
 
@@ -21,6 +22,14 @@ static void WriteSingleStringToClipboard(const std::string &_s)
     NSPasteboard *const pb = NSPasteboard.generalPasteboard;
     [pb declareTypes:@[NSPasteboardTypeString] owner:nil];
     [pb setString:[NSString stringWithUTF8StdString:_s] forType:NSPasteboardTypeString];
+}
+
+static std::string JoinItemStrings(const std::vector<VFSListingItem> &_entries,
+                                   const std::function<std::string(const VFSListingItem &)> &_projection)
+{
+    return std::accumulate(std::begin(_entries), std::end(_entries), std::string{}, [&](const auto &a, const auto &b) {
+        return a + (a.empty() ? "" : Separator()) + _projection(b);
+    });
 }
 
 bool CopyFileName::Predicate(PanelController *_source) const
@@ -41,31 +50,54 @@ bool CopyFileDirectory::Predicate(PanelController *_source) const
 void CopyFileName::Perform(PanelController *_source, id /*_sender*/) const
 {
     const auto entries = _source.selectedEntriesOrFocusedEntry;
-    const auto result =
-        std::accumulate(std::begin(entries), std::end(entries), std::string{}, [](const auto &a, const auto &b) {
-            return a + (a.empty() ? "" : Separator()) + b.Filename();
-        });
-    WriteSingleStringToClipboard(result);
+    WriteSingleStringToClipboard(JoinItemStrings(entries, [](const auto &item) { return item.Filename(); }));
 }
 
 void CopyFilePath::Perform(PanelController *_source, id /*_sender*/) const
 {
     const auto entries = _source.selectedEntriesOrFocusedEntry;
-    const auto result =
-        std::accumulate(std::begin(entries), std::end(entries), std::string{}, [](const auto &a, const auto &b) {
-            return a + (a.empty() ? "" : Separator()) + b.Path();
-        });
-    WriteSingleStringToClipboard(result);
+    WriteSingleStringToClipboard(JoinItemStrings(entries, [](const auto &item) { return item.Path(); }));
 }
 
 void CopyFileDirectory::Perform(PanelController *_source, id /*_sender*/) const
 {
     const auto entries = _source.selectedEntriesOrFocusedEntry;
-    const auto result =
-        std::accumulate(std::begin(entries), std::end(entries), std::string{}, [](const auto &a, const auto &b) {
-            return a + (a.empty() ? "" : Separator()) + b.Directory();
-        });
-    WriteSingleStringToClipboard(result);
+    WriteSingleStringToClipboard(JoinItemStrings(entries, [](const auto &item) { return item.Directory(); }));
+}
+
+context::CopyPathname::CopyPathname(const std::vector<VFSListingItem> &_items) : m_Items(_items)
+{
+    if( _items.empty() )
+        throw std::invalid_argument("CopyPathname was made with empty items set");
+}
+
+bool context::CopyPathname::Predicate([[maybe_unused]] PanelController *_source) const
+{
+    return !m_Items.empty();
+}
+
+bool context::CopyPathname::ValidateMenuItem([[maybe_unused]] PanelController *_source, NSMenuItem *_item) const
+{
+    if( m_Items.size() > 1 ) {
+        _item.title =
+            [NSString stringWithFormat:NSLocalizedStringFromTable(@"Copy %lu Items as Pathnames",
+                                                                  @"FilePanelsContextMenu",
+                                                                  "Copy many items as plain-text pathnames"),
+                                     m_Items.size()];
+    }
+    else {
+        _item.title = [NSString stringWithFormat:NSLocalizedStringFromTable(
+                                                     @"Copy “%@” as Pathname",
+                                                     @"FilePanelsContextMenu",
+                                                     "Copy one item as a plain-text pathname"),
+                                                 m_Items.front().DisplayNameNS()];
+    }
+    return Predicate(_source);
+}
+
+void context::CopyPathname::Perform([[maybe_unused]] PanelController *_source, id /*_sender*/) const
+{
+    WriteSingleStringToClipboard(JoinItemStrings(m_Items, [](const auto &item) { return item.Path(); }));
 }
 
 } // namespace nc::panel::actions

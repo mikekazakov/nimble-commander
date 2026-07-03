8bf758639b0398b8866d41804530b89fdd18480e
Michael Kazakov <mike.kazakov@gmail.com>
2026-07-03 21:50:53 +0100

    Fixed the busy loop

--- Source/VFS/source/NetFTP/File.cpp
+++ Source/VFS/source/NetFTP/File.cpp
@@ -375,11 +375,17 @@ void File::FinishReading()
       return 1;
     };
 
-    int running_handles = 0;
+    int still_running = 0;
     do {
-        while( CURLM_CALL_MULTI_PERFORM == curl_multi_perform(m_CURL->curlm, &running_handles) )
-            ;
-    } while( running_handles );
+        CURLMcode mc = curl_multi_perform(m_CURL->curlm, &still_running);
+        if( mc == CURLM_OK ) {
+            mc = curl_multi_wait(m_CURL->curlm, nullptr, 0, m_SelectTimeout.tv_usec, nullptr);
+        }
+        if( mc != CURLM_OK ) {
+            Log::Error("curl_multi failed, code {}.", std::to_underlying(mc));
+            break;
+        }
+    } while( still_running );
 }
 
 } // namespace nc::vfs::ftp
